882080bd608ec1c571cda2824aaef482ccc61a11
kathy-huang <kathy.huang@datadoghq.com>
2019-03-22 01:29:43 -0400

    Fix broken Reconcile test to check for created inject+cleanup pods

--- pkg/controller/dependencyfailureinjection/dependencyfailureinjection_controller_test.go
+++ pkg/controller/dependencyfailureinjection/dependencyfailureinjection_controller_test.go
@@ -34,14 +34,17 @@ import (
 
 var c client.Client
 
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo-pod", Namespace: "default"}
+var (
+	expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
+	dfiKey          = types.NamespacedName{Name: "foo", Namespace: "default"}
+	injectPodKey    = types.NamespacedName{Name: "foo-inject-foo-pod-pod", Namespace: "default"}
+	cleanupPodKey   = types.NamespacedName{Name: "foo-cleanup-foo-pod-pod", Namespace: "default"}
+)
 
 const timeout = time.Second * 5
 
 func TestReconcile(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	instance := &chaosv1beta1.DependencyFailureInjection{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
 
 	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
 	// channel when it is finished.
@@ -59,7 +62,42 @@ func TestReconcile(t *testing.T) {
 		mgrStopped.Wait()
 	}()
 
-	// Create the DependencyFailureInjection object and expect the Reconcile and Deployment to be created
+	// Create a pod for the DependencyFailureInjection to target
+	targetPod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-pod",
+			Namespace: "default",
+			Labels: map[string]string{
+				"foo": "bar",
+			},
+		},
+	}
+	err = c.Create(context.TODO(), targetPod)
+	// The instance object may not be a valid object because it might be missing some required fields.
+	// Please modify the instance object by adding required fields and then remove the following if statement.
+	if apierrors.IsInvalid(err) {
+		t.Logf("failed to create object, got an invalid object error: %v", err)
+		return
+	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	defer c.Delete(context.TODO(), targetPod)
+
+	// Create the DependencyFailureInjection object and expect the Reconcile request and inject pod to be created
+	instance := &chaosv1beta1.DependencyFailureInjection{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: chaosv1beta1.DependencyFailureInjectionSpec{
+			LabelSelector: "foo=bar",
+			Failure: chaosv1beta1.DependencyFailureInjectionSpecFailure{
+				Host:        "127.0.0.1",
+				Port:        80,
+				Probability: 0,
+				Protocol:    "tcp",
+			},
+		},
+	}
 	err = c.Create(context.TODO(), instance)
 	// The instance object may not be a valid object because it might be missing some required fields.
 	// Please modify the instance object by adding required fields and then remove the following if statement.
@@ -68,21 +106,26 @@ func TestReconcile(t *testing.T) {
 		return
 	}
 	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), instance)
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
 
-	pod := &corev1.Pod{}
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, pod) }, timeout).
+	injectPod := &corev1.Pod{}
+	g.Eventually(func() error { return c.Get(context.TODO(), injectPodKey, injectPod) }, timeout).
 		Should(gomega.Succeed())
+	defer c.Delete(context.TODO(), injectPod)
 
-	// Delete the Deployment and expect Reconcile to be called for Deployment deletion
-	g.Expect(c.Delete(context.TODO(), pod)).NotTo(gomega.HaveOccurred())
+	// Delete the DFI and expect Reconcile to be called for the DFI deletion,
+	// and expect the cleanup pod to be created
+	g.Expect(c.Delete(context.TODO(), instance)).NotTo(gomega.HaveOccurred())
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, pod) }, timeout).
+	g.Eventually(func() error { return c.Get(context.TODO(), dfiKey, instance) }, timeout).
 		Should(gomega.Succeed())
+	cleanupPod := &corev1.Pod{}
+	g.Eventually(func() error { return c.Get(context.TODO(), cleanupPodKey, cleanupPod) }, timeout).
+		Should(gomega.Succeed())
+	defer c.Delete(context.TODO(), cleanupPod)
 
-	// Manually delete Deployment since GC isn't enabled in the test control plane
-	g.Eventually(func() error { return c.Delete(context.TODO(), pod) }, timeout).
-		Should(gomega.MatchError("pods \"foo-pod\" not found"))
+	// Manually delete DFI since GC isn't enabled in the test control plane
+	g.Eventually(func() error { return c.Delete(context.TODO(), instance) }, timeout).
+		Should(gomega.MatchError("DependencyFailureInjection \"foo\" not found"))
 
 }
