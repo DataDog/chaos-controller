a76a0f5ee160f64c0727875e8b13400bf2683ddb
kathy-huang <kathy.huang@datadoghq.com>
2019-04-01 19:06:38 -0400

    rename DependencyFailureInjection -> NetworkFailureInjection

--- pkg/controller/networkfailureinjection/networkfailureinjection_controller_suite_test.go
+++ pkg/controller/networkfailureinjection/networkfailureinjection_controller_suite_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package dependencyfailureinjection
+package networkfailureinjection
 
 import (
 	stdlog "log"
