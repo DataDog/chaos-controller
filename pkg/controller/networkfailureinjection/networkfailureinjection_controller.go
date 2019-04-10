d9001147e217811ac497f1b92a1219974b78a106
Devatoria <joris.bonnefoy@datadoghq.com>
2019-04-10 16:34:58 +0200

    Pass instance UID to chaos-fi pods

--- pkg/controller/networkfailureinjection/networkfailureinjection_controller.go
+++ pkg/controller/networkfailureinjection/networkfailureinjection_controller.go
@@ -234,6 +234,8 @@ func (r *ReconcileNetworkFailureInjection) Reconcile(request reconcile.Request) 
 						Args: []string{
 							"inject",
 							"network-failure",
+							"--uid",
+							string(instance.ObjectMeta.UID),
 							"--container-id",
 							containerID,
 							"--host",
@@ -353,7 +355,7 @@ func (r *ReconcileNetworkFailureInjection) cleanFailures(instance *chaosv1beta1.
 						Image:           os.Getenv(ChaosFailureInjectionImageVariableName),
 						ImagePullPolicy: "Always",
 						Command:         []string{"cmd"},
-						Args:            []string{"clean", "--container-id", containerID},
+						Args:            []string{"clean", "--uid", string(instance.ObjectMeta.UID), "--container-id", containerID},
 						VolumeMounts: []corev1.VolumeMount{
 							corev1.VolumeMount{
 								MountPath: "/run/containerd",
