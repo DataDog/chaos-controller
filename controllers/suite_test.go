827164ce93e80b4aed9b686e3ba7169f2da46054
Devatoria <joris.bonnefoy@datadoghq.com>
2020-01-09 16:32:28 +0100

    Add node failure tests

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -16,13 +16,22 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"bou.ke/monkey"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	chaosv1beta1 "github.com/DataDog/chaos-fi-controller/api/v1beta1"
+	"github.com/DataDog/chaos-fi-controller/datadog"
+	"github.com/DataDog/datadog-go/statsd"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -36,10 +45,18 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var k8sManager ctrl.Manager
-var testEnv *envtest.Environment
+const (
+	timeout = time.Second * 5
+)
+
+var (
+	cfg         *rest.Config
+	k8sClient   client.Client
+	k8sManager  ctrl.Manager
+	testEnv     *envtest.Environment
+	instanceKey types.NamespacedName
+	targetPod   *corev1.Pod
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -78,8 +95,16 @@ var _ = BeforeSuite(func(done Done) {
 
 	err = (&NetworkFailureInjectionReconciler{
 		Client:   k8sManager.GetClient(),
-		Log:      ctrl.Log.WithName("controllers").WithName("SecretScope"),
-		Recorder: k8sManager.GetEventRecorderFor("secretscope-controller"),
+		Log:      ctrl.Log.WithName("controllers").WithName("NetworkFailureInjection"),
+		Recorder: k8sManager.GetEventRecorderFor("networkfailureinjection-controller"),
+		Scheme:   scheme.Scheme,
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&NodeFailureInjectionReconciler{
+		Client:   k8sManager.GetClient(),
+		Log:      ctrl.Log.WithName("controllers").WithName("NodeFailureInjection"),
+		Recorder: k8sManager.GetEventRecorderFor("nodefailureinjection-controller"),
 		Scheme:   scheme.Scheme,
 	}).SetupWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
@@ -95,8 +120,46 @@ var _ = BeforeSuite(func(done Done) {
 	close(done)
 }, 60)
 
+var _ = BeforeEach(func() {
+	instanceKey = types.NamespacedName{Name: "foo", Namespace: "default"}
+	targetPod = &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-pod",
+			Namespace: "default",
+			Labels: map[string]string{
+				"foo": "bar",
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				corev1.Container{
+					Image: "foo",
+					Name:  "foo",
+				},
+			},
+		},
+	}
+
+	By("Creating target pod")
+	err := k8sClient.Create(context.Background(), targetPod)
+	if apierrors.IsInvalid(err) {
+		logf.Log.Error(err, "failed to create object, got an invalid object error")
+		return
+	}
+	Expect(err).NotTo(HaveOccurred())
+
+	logf.Log.Info("patching datadog instance")
+	monkey.Patch(datadog.GetInstance, func() *statsd.Client {
+		return nil
+	})
+})
+
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterEach(func() {
+	k8sClient.Delete(context.Background(), targetPod)
 })
