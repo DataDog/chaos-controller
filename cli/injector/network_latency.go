31c013cb81cbdfd2e67967ea721222f57871a4db
Devatoria <joris.bonnefoy@datadoghq.com>
2020-01-30 11:00:20 +0100

    Fix golangci-lint feedbacks

--- cli/injector/network_latency.go
+++ cli/injector/network_latency.go
@@ -13,5 +13,5 @@ func init() {
 	networkLatencyCmd.AddCommand(networkLatencyCleanCmd)
 	networkLatencyCmd.PersistentFlags().String("container-id", "", "ID of the container to inject/clean")
 	networkLatencyCmd.PersistentFlags().StringSlice("hosts", []string{}, "List of hosts (hostname, single IP or IP block) to apply delay to. If not specified, the delay applies to all the outgoing traffic")
-	cobra.MarkFlagRequired(networkFailureCmd.PersistentFlags(), "container-id")
+	_ = cobra.MarkFlagRequired(networkFailureCmd.PersistentFlags(), "container-id")
 }
