a056a3e3eecf8157143e3d74da808c2690a4abce
Devatoria <joris.bonnefoy@datadoghq.com>
2020-01-31 18:37:07 +0100

    Add 3rd party licenses and generation script

---
+++ tasks/3rdparty.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import csv
+import spdx_lookup as lookup
+import os
+import glob
+
+# read deps from go.sum file
+deps = {}
+with open('vendor/modules.txt', 'r') as modules:
+    parent = ''
+    for line in modules.readlines():
+        if line.startswith('#'):
+            parent = line.split()[1]
+        else:
+            if parent not in deps:
+                deps[parent] = []
+            deps[parent].append(line.strip())
+
+# write deps to csv
+with open('LICENSE-3rdparty.csv', 'w', newline='') as csvfile:
+    w = csv.DictWriter(csvfile, ['From', 'Package', 'License'])
+    w.writeheader()
+    for parent, packages in deps.items():
+        # find license file
+        base_path = 'vendor/{}'.format(parent)
+        license_filename = None
+        if os.path.isfile('{}/LICENSE'.format(base_path)):
+            license_filename = '{}/LICENSE'.format(base_path)
+        else:
+            license_files = glob.glob('{}/LICENSE.*'.format(base_path))
+            if len(license_files) > 1:
+                print('error: multiple license files for package {}'.format(parent))
+            elif len(license_files) == 0:
+                print('error: could not find license file for package {}'.format(parent))
+            else:
+                license_filename = license_files[0]
+
+        # determine license type
+        license_type = 'Unknown'
+        if license_filename is not None:
+            with open('{}'.format(license_filename), 'r') as license_file:
+                match = lookup.match(license_file.read())
+                if match is None:
+                    print('error: could not determine license type for package {}'.format(parent))
+                else:
+                    license_type = match.license
+
+        # write rows for child packages
+        for package in packages:
+                w.writerow({
+                    'From': parent,
+                    'Package': package,
+                    'License': license_type,
+                })
