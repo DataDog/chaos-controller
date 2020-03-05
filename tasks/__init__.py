45426460867c52a4b5219bc0ea582c96434cedde
Jamie van Brunschot <jamievbrunschot@gmail.com>
2020-03-05 16:01:46 +0100

    move license check to invoke

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,12 +3,12 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2020 Datadog, Inc.
 
-"""
-Invoke entrypoint, import here all the tasks we want to make available
-"""
 from invoke import  Collection
+
 from .header import header_check
+from .thirdparty import license_check
 
 ns = Collection()
 
 ns.add_task(header_check)
+ns.add_task(license_check)
