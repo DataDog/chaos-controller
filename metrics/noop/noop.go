790afa21cf6c5ad90ce3eda879191fbf2239783e
Jamie van Brunschot <jamievbrunschot@gmail.com>
2020-03-06 16:36:27 +0100

    remove noop prints

--- metrics/noop/noop.go
+++ metrics/noop/noop.go
@@ -6,8 +6,6 @@
 package noop
 
 import (
-	"log"
-
 	"github.com/DataDog/chaos-controller/types"
 )
 
@@ -20,36 +18,26 @@ func New() *Sink {
 }
 
 // EventWithTags creates a new event with the given title, text and tags and send it
-func (n *Sink) EventWithTags(title, text string, tags []string) {
-	log.Printf("%v %v %v", title, text, tags)
-}
+func (n *Sink) EventWithTags(title, text string, tags []string) {}
 
 // EventCleanFailure sends an event to datadog specifying a failure clean fail
-func (n *Sink) EventCleanFailure(containerID, uid string) {
-	log.Printf("EventCleanFailure %v", containerID)
-}
+func (n *Sink) EventCleanFailure(containerID, uid string) {}
 
 // EventInjectFailure sends an event to datadog specifying a failure inject fail
-func (n *Sink) EventInjectFailure(containerID, uid string) {
-	log.Printf("EventInjectFailure %v", containerID)
-}
+func (n *Sink) EventInjectFailure(containerID, uid string) {}
 
 // MetricInjected increments the injected metric
 func (n *Sink) MetricInjected(containerID, uid string, succeed bool, kind types.DisruptionKind, tags []string) {
-	log.Printf("MetricInjected %v", containerID)
 }
 
 // MetricRulesInjected rules.increments the injected metric
 func (n *Sink) MetricRulesInjected(containerID, uid string, succeed bool, kind types.DisruptionKind, tags []string) {
-	log.Printf("MetricRulesInjected %v", containerID)
 }
 
 // MetricCleaned increments the cleaned metric
 func (n *Sink) MetricCleaned(containerID, uid string, succeed bool, kind types.DisruptionKind, tags []string) {
-	log.Printf("MetricCleaned %v", containerID)
 }
 
 // MetricIPTablesRulesInjected increment iptables_rules metrics
 func (n *Sink) MetricIPTablesRulesInjected(containerID, uid string, succeed bool, kind types.DisruptionKind, tags []string) {
-	log.Printf("MetricIPTablesRulesInjected %v", containerID)
 }
