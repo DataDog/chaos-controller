523618a97337b58b39090adeb156c4ba5963251e
Jamie van Brunschot <7092893+jvanbrunschot@users.noreply.github.com>
2020-03-06 11:08:49 +0100

    Improve task management (#82)
    * introduce invoke as task manager
    
    * update changelog
    
    * move license check to invoke

---
+++ tasks/__init__.py
@@ -0,0 +1,14 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2020 Datadog, Inc.
+
+from invoke import  Collection
+
+from .header import header_check
+from .thirdparty import license_check
+
+ns = Collection()
+
+ns.add_task(header_check)
+ns.add_task(license_check)
