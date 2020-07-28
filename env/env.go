9f278c3fc44c232b41269615cd021b725969e2e7
Devatoria <joris.bonnefoy@datadoghq.com>
2020-07-28 12:46:06 +0200

    Rework and clean up injector mount points

---
+++ env/env.go
@@ -0,0 +1,9 @@
+package env
+
+const (
+	InjectorMountHost         = "CHAOS_INJECTOR_MOUNT_HOST"
+	InjectorMountProc         = "CHAOS_INJECTOR_MOUNT_PROC"
+	InjectorMountCgroup       = "CHAOS_INJECTOR_MOUNT_CGROUP"
+	InjectorMountSysrq        = "CHAOS_INJECTOR_MOUNT_SYSRQ"
+	InjectorMountSysrqTrigger = "CHAOS_INJECTOR_MOUNT_SYSRQ_TRIGGER"
+)
