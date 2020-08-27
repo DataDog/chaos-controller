3d61c53aa27c88d07f4c6119e9608f395609e9fb
Joris Bonnefoy <joris.bonnefoy@datadoghq.com>
2020-08-27 16:26:47 +0200

    Simplify reconcile loop and update vendors (#157)

---
+++ api/generator.go
@@ -0,0 +1,16 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2020 Datadog, Inc.
+
+package api
+
+import (
+	chaostypes "github.com/DataDog/chaos-controller/types"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// DisruptionArgsGenerator generates args for the given disruption
+type DisruptionArgsGenerator interface {
+	GenerateArgs(mode chaostypes.PodMode, uid types.UID, containerID, sink string) []string
+}
