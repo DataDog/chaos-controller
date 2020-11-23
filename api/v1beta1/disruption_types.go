856c9a0ff27f68381cfe16ad08070b82858b1e68
Sam Azouzi <sam.azouzi@datadoghq.com>
2020-11-23 12:56:46 -0500

    Adding the ability to target specific container in targeted Pod (#189)
    * adding the ability to target specific container in targeted Pod
    
    * fixing linting issues
    
    * add container choice to docs
    
    * omitempty and example
    
    * fix incorrect nullable removal
    
    * fix linting issues
    
    * update manifests

--- api/v1beta1/disruption_types.go
+++ api/v1beta1/disruption_types.go
@@ -34,7 +34,8 @@ type DisruptionSpec struct {
 	// +kubebuilder:validation:Required
 	Selector labels.Set `json:"selector"` // label selector
 	// +kubebuilder:validation:Enum=pod;node;""
-	Level chaostypes.DisruptionLevel `json:"level,omitempty"`
+	Level     chaostypes.DisruptionLevel `json:"level,omitempty"`
+	Container string                     `json:"container,omitempty"`
 	// +nullable
 	Network *NetworkDisruptionSpec `json:"network,omitempty"`
 	// +nullable
