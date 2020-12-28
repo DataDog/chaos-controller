a93d329de72231d7c42eed50128fbb4663bbbb77
Philip Thompson <philip.thompson@datadoghq.com>
2020-12-28 12:07:04 -0500

    CORE-414: Validate label selector grammar (#208)
    * CORE-414: Add safeguard for invalid characters in label selector
    
    * Extract validateLabelSelector
    
    * Move controller functions into helpers.go

---
+++ controllers/helpers_test.go
@@ -0,0 +1,55 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2020 Datadog, Inc.
+
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+var _ = Describe("Label Selector Validation", func() {
+
+	Context("validating an empty label selector", func() {
+		It("", func() {
+			selector := labels.Set{}
+			Expect(validateLabelSelector(selector.AsSelector())).ToNot(BeNil())
+		})
+	})
+	Context("validating a good label selector", func() {
+		It("", func() {
+			selector := labels.Set{"foo": "bar"}
+			Expect(validateLabelSelector(selector.AsSelector())).To(BeNil())
+		})
+	})
+	Context("validating special characters in label selector", func() {
+		It("", func() {
+			selector := labels.Set{"foo": "”bar”"}
+			Expect(validateLabelSelector(selector.AsSelector())).ToNot(BeNil())
+		})
+	})
+	Context("validating too many quotes in label selector", func() {
+		It("", func() {
+			selector := labels.Set{"foo": "\"bar\""}
+			Expect(validateLabelSelector(selector.AsSelector())).ToNot(BeNil())
+		})
+	})
+})
