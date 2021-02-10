9c08832a641a259d4d1c90d53c23f6c566417c1c
Joris Bonnefoy <joris.bonnefoy@datadoghq.com>
2021-02-10 14:48:08 +0100

    Generate injector pods from code instead of from template (#249)

--- main.go
+++ main.go
@@ -21,9 +21,6 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"io/ioutil"
 	"os"
 
 	chaosv1beta1 "github.com/DataDog/chaos-controller/api/v1beta1"
@@ -31,7 +28,7 @@ import (
 	"github.com/DataDog/chaos-controller/log"
 	"github.com/DataDog/chaos-controller/metrics"
 	"github.com/DataDog/chaos-controller/metrics/types"
-	corev1 "k8s.io/api/core/v1"
+	"github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -54,17 +51,15 @@ func main() {
 	var (
 		metricsAddr          string
 		enableLeaderElection bool
-		podTemplate          string
-		podTemplateSpec      corev1.Pod
 		sink                 string
+		injectorAnnotations  map[string]string
 	)
 
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-	flag.StringVar(&podTemplate, "pod-template", "/etc/manager/pod-template.json", "The template file to use to generate injection pods.")
-	flag.StringVar(&sink, "metrics-sink", "noop", "Metrics sink (datadog, or noop)")
-	flag.Parse()
+	pflag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	pflag.BoolVar(&enableLeaderElection, "enable-leader-election", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	pflag.StringVar(&sink, "metrics-sink", "noop", "Metrics sink (datadog, or noop)")
+	pflag.StringToStringVar(&injectorAnnotations, "injector-annotations", map[string]string{}, "Annotations added to the generated injector pods")
+	pflag.Parse()
 
 	logger, err := log.NewZapLogger()
 	if err != nil {
@@ -99,30 +94,15 @@ func main() {
 		}
 	}()
 
-	// load pod template
-	bytes, err := ioutil.ReadFile(podTemplate) //nolint:gosec
-	if err != nil {
-		ctrl.Log.Error(err, "unable to read pod template file")
-		os.Exit(1)
-	}
-
-	err = json.Unmarshal(bytes, &podTemplateSpec)
-	if err != nil {
-		ctrl.Log.Error(err, "unable to load pod template spec")
-		os.Exit(1)
-	}
-
-	ctrl.Log.Info("generated pod template", "template", podTemplateSpec)
-
 	// create reconciler
 	r := &controllers.DisruptionReconciler{
-		Client:          mgr.GetClient(),
-		BaseLog:         logger,
-		Scheme:          mgr.GetScheme(),
-		Recorder:        mgr.GetEventRecorderFor("disruption-controller"),
-		MetricsSink:     ms,
-		PodTemplateSpec: podTemplateSpec,
-		TargetSelector:  controllers.RunningTargetSelector{},
+		Client:              mgr.GetClient(),
+		BaseLog:             logger,
+		Scheme:              mgr.GetScheme(),
+		Recorder:            mgr.GetEventRecorderFor("disruption-controller"),
+		MetricsSink:         ms,
+		TargetSelector:      controllers.RunningTargetSelector{},
+		InjectorAnnotations: injectorAnnotations,
 	}
 
 	if err := r.SetupWithManager(mgr); err != nil {
