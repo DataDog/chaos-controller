71f70f14be0428fb72834f627b4e1853e0081b66
Philip Thompson <philip.thompson@datadoghq.com>
2021-05-12 13:52:03 -0400

    Fix imports and copyrights; then run go mod tidy

--- cli/chaosli/cmd/create.go
+++ cli/chaosli/cmd/create.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2021 Datadog, Inc.
+
 package cmd
 
 import (
@@ -6,7 +11,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// helloCmd represents the hello command
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "create a disruption.",
