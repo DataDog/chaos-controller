0a2bb1a649fa92585fc7e1b8df19df8120862bd3
Nathan Tournant <nathan.tournant@datadoghq.com>
2021-05-14 13:26:22 +0200

    Validation: tryout for scope-level validation
    Validation: removed useless variable prior to refactoring
    
    Validation: code edit, factorisation
    
    Validation: linted
    
    Validation: admission controller applies all validation rules
    
    Validation: semantic fix, remove useless check
    
    Validation: validator and argsgenerator interface merge
    
    Validation: fix call to DisruptionKindPicker, renamed disruption_spec
    
    Validation: fixed naming for DisruptionType and DisruptionTypeName
    
    Validation: removed blocking rules for the test
    
    The disruption_controller test file has to be reworked wholly as it isn't fit to test global restrictions, such as the DNS and Network incompatibility rule
    
    Validation: godoc comment adapt and CRD file rename
    
    CORE-702: added validation from CLI

--- cli/chaosli/cmd/validate.go
+++ cli/chaosli/cmd/validate.go
@@ -7,7 +7,14 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 
+	goyaml "github.com/ghodss/yaml"
+
+	"github.com/DataDog/chaos-controller/api/v1beta1"
 	"github.com/spf13/cobra"
 )
 
@@ -15,10 +22,11 @@ var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "validate disruption config",
 	Long:  `validates the yaml of the disruption for structure.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	PreRunE: func(cmd *cobra.Command, args []string) error {
 		path, _ := cmd.Flags().GetString("path")
-		fmt.Println(validate(path))
+		return validatePath(path)
 	},
+	Run: ValidateDisruption,
 }
 
 func validate(filePath string) string {
@@ -31,4 +39,43 @@ func validate(filePath string) string {
 
 func init() {
 	validateCmd.Flags().String("path", "", "The path to the disruption file to be validated.")
+}
+
+func ValidateDisruption(cmd *cobra.Command, args []string) {
+	path, _ := cmd.Flags().GetString("path")
+	disruption, _ := DisruptionFromFile(path)
+
+	err := disruption.Spec.Validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func validatePath(filePath string) error {
+	if filePath == "" {
+		return fmt.Errorf("no path given, exiting")
+	}
+
+	return nil
+}
+
+func DisruptionFromFile(path string) (v1beta1.Disruption, error) {
+	yaml, err := os.Open(filepath.Clean(path))
+	if err != nil {
+		return v1beta1.Disruption{}, err
+	}
+
+	yamlBytes, err := ioutil.ReadAll(yaml)
+	if err != nil {
+		return v1beta1.Disruption{}, err
+	}
+
+	parsedSpec := v1beta1.Disruption{}
+	err = goyaml.Unmarshal(yamlBytes, &parsedSpec)
+
+	if err != nil {
+		return v1beta1.Disruption{}, err
+	}
+
+	return parsedSpec, nil
 }
