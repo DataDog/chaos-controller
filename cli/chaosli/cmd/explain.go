acc15d803dfafb066141af7a7530c50213b69821
Philip Thompson <philip.thompson@datadoghq.com>
2021-05-26 15:28:37 -0400

    initial review feedback

--- cli/chaosli/cmd/explain.go
+++ cli/chaosli/cmd/explain.go
@@ -12,169 +12,97 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/DataDog/chaos-controller/api/v1beta1"
+	chaostypes "github.com/DataDog/chaos-controller/types"
+	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
-	"k8s.io/apimachinery/pkg/labels"
 )
 
-type NodeFailure struct {
-	Shutdown *bool `yaml:"shutdown"`
-}
-
-type Network struct {
-	Hosts          []string `yaml:"hosts"`
-	Port           *int     `yaml:"port"`
-	Protocol       *string  `yaml:"protocol"`
-	Flow           *string  `yaml:"flow"`
-	Drop           *int     `yaml:"drop"`
-	Corrupt        *int     `yaml:"corrupt"`
-	Delay          *int     `yaml:"delay"`
-	DelayJitter    *int     `yaml:"delayJitter"`
-	BandwidthLimit *int     `yaml:"bandwidthLimit"`
-}
-
-type CPUPressure struct{}
-
-type Throttling struct {
-	ReadBytesPerSec  *int `yaml:"readBytesPerSec"`
-	WriteBytesPerSec *int `yaml:"writeBytesPerSec"`
-}
-type DiskPressure struct {
-	Path       *string     `yaml:"path"`
-	Throttling *Throttling `yaml:"throttling"`
-}
-
-type Record struct {
-	Type  *string `yaml:"type"`
-	Value *string `yaml:"value"`
-}
-type DNS struct {
-	Hostname *string `yaml:"hostname"`
-	Record   *Record `yaml:"record"`
-}
-
-type Spec struct {
-	DryRun       *bool         `yaml:"dryRun"`
-	Level        *string       `yaml:"level"`
-	Selector     labels.Set    `yaml:"selector"`
-	Containers   []string      `yaml:"containers"`
-	Count        *string       `yaml:"count"`
-	NodeFailure  *NodeFailure  `yaml:"nodeFailure"`
-	Network      *Network      `yaml:"network"`
-	CPUPressure  *CPUPressure  `yaml:"cpuPressure"`
-	DiskPressure *DiskPressure `yaml:"diskPressure"`
-	DNS          []DNS         `yaml:"dns"`
-}
-
-type DisruptionSpec struct {
-	ApiVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Metadata   struct {
-		Name      string `yaml:"name"`
-		Namespace string `yaml:"namespace"`
-	} `yaml:"metadata"`
-	Spec Spec `yaml:"spec"`
-}
-
 type Explanations struct {
-	seperator string
+	separator string
 }
 
-func (e *Explanations) printSeperator() {
-	fmt.Println(e.seperator)
+func (e *Explanations) printSeparator() {
+	fmt.Println(e.separator)
 }
 
-func (e *Explanations) explainMetaSpec(spec Spec) {
-	e.printSeperator()
+func (e *Explanations) explainMetaSpec(spec v1beta1.DisruptionSpec) {
+	e.printSeparator()
 	fmt.Println("🧰 has the following metadata  ...")
-	if spec.DryRun != nil {
-		if *spec.DryRun == true {
-			fmt.Println("\tℹ️  has DryRun set to true meaning no actual disruption is being run.")
-		} else {
-			fmt.Println("\tℹ️  has DryRun set to false meaning this disruption WILL run.")
-		}
+	if spec.DryRun {
+		fmt.Println("\tℹ️  has DryRun set to true meaning no actual disruption is being run.")
+	} else {
+		fmt.Println("\tℹ️  has DryRun set to false meaning this disruption WILL run.")
 	}
 
-	level := "pod/node"
-	if spec.Level != nil {
-		if *spec.Level == "pod" {
-			level = "pod"
-			fmt.Println("\tℹ️  will be run on the Pod level, so everything in this disruption is scoped at this level.")
-		} else if *spec.Level == "node" {
-			level = "node"
-			fmt.Println("\tℹ️  will be run on the Node level, so everything in this disruption is scoped at this level.")
-		} else {
-			fmt.Println("\tℹ️  level is unknown and will most likely cause errors when applied.")
-		}
+	if spec.Level == chaostypes.DisruptionLevelPod {
+		fmt.Println("\tℹ️  will be run on the Pod level, so everything in this disruption is scoped at this level.")
+	} else if spec.Level == chaostypes.DisruptionLevelNode {
+		fmt.Println("\tℹ️  will be run on the Node level, so everything in this disruption is scoped at this level.")
+	} else {
+		fmt.Println("\tℹ️  level is unknown and will most likely cause errors when applied.")
 	}
 
 	if spec.Selector != nil {
-		fmt.Printf("\tℹ️  has the following selectors which will be used to target %ss\n\t\t🎯  %s\n", level, spec.Selector.String())
+		fmt.Printf("\tℹ️  has the following selectors which will be used to target %ss\n\t\t🎯  %s\n", spec.Level, spec.Selector.String())
 	}
 
 	if spec.Containers != nil {
-		if level == "node" {
+		if spec.Level == chaostypes.DisruptionLevelNode {
 			fmt.Println("\tℹ️  is using the node level. The Containers attribute only makes sense when using the pod level!")
 		}
 		fmt.Printf("\tℹ️  will target the following containers when targeting on the pod level\n\t\t🎯  %s\n", strings.Join(spec.Containers, ","))
 	}
 
-	if spec.Count != nil {
-		fmt.Printf("\tℹ️  is going to target %s %s (either described as a percentage of total %ss or actual number of them).\n", *spec.Count, level, level)
-	}
-	e.printSeperator()
+	fmt.Printf("\tℹ️  is going to target %s %s (either described as a percentage of total %ss or actual number of them).\n", spec.Count, spec.Level, spec.Level)
+	e.printSeparator()
 }
 
-func (e *Explanations) explainNodeFailure(spec Spec) {
+func (e *Explanations) explainNodeFailure(spec v1beta1.DisruptionSpec) {
 	nodeFailure := spec.NodeFailure
 	if nodeFailure == nil {
 		return
 	}
-	if nodeFailure.Shutdown != nil {
-		if *nodeFailure.Shutdown == true {
-			fmt.Println("💉 injects a node failure which shuts down the host (violently) instead of triggering a kernel panic so the host is kept down and not restarted.")
-		} else {
-			fmt.Println("💉 injects a node failure which triggers a kernel panic on the node.")
-		}
+	if nodeFailure.Shutdown {
+		fmt.Println("💉 injects a node failure which shuts down the host (violently) instead of triggering a kernel panic so the host is kept down and not restarted.")
+	} else {
+		fmt.Println("💉 injects a node failure which triggers a kernel panic on the node.")
 	}
-	e.printSeperator()
+	e.printSeparator()
 }
 
-func (e *Explanations) explainCPUPressure(spec Spec) {
+func (e *Explanations) explainCPUPressure(spec v1beta1.DisruptionSpec) {
 	cpuPressure := spec.CPUPressure
 	if cpuPressure == nil {
 		return
 	}
 	fmt.Println("💉 injects a cpu pressure disruption ...")
-	e.printSeperator()
+	e.printSeparator()
 }
 
-func (e *Explanations) explainDiskPressure(spec Spec) {
+func (e *Explanations) explainDiskPressure(spec v1beta1.DisruptionSpec) {
 	diskPressure := spec.DiskPressure
 	if diskPressure == nil {
 		return
 	}
 	fmt.Println("💉 injects a disk pressure disruption ...")
-	if diskPressure.Path == nil {
+	if diskPressure.Path == "" {
 		fmt.Println("\t⚠️ no path specified for disk disruption, this is required!!")
 	} else {
-		fmt.Printf("\t🗂  on path %s\n", *diskPressure.Path)
+		fmt.Printf("\t🗂  on path %s\n", diskPressure.Path)
 	}
-	if diskPressure.Throttling == nil {
-		fmt.Println("\t⚠️ no throttling specified for disk disruption, this is required!!")
-	} else {
-		fmt.Println("\t🏃🏾‍♀️ with the following thresholds...")
-		if diskPressure.Throttling.ReadBytesPerSec != nil {
-			fmt.Printf("\t\t📖 %d read bytes per second\n", *diskPressure.Throttling.ReadBytesPerSec)
-		}
-		if diskPressure.Throttling.WriteBytesPerSec != nil {
-			fmt.Printf("\t\t📝 %d write bytes per second\n", *diskPressure.Throttling.WriteBytesPerSec)
-		}
+	fmt.Println("\t🏃🏾‍♀️ with the following thresholds...")
+	if diskPressure.Throttling.ReadBytesPerSec != nil {
+		fmt.Printf("\t\t📖 %d read bytes per second\n", *diskPressure.Throttling.ReadBytesPerSec)
 	}
-	e.printSeperator()
+	if diskPressure.Throttling.WriteBytesPerSec != nil {
+		fmt.Printf("\t\t📝 %d write bytes per second\n", *diskPressure.Throttling.WriteBytesPerSec)
+	}
+
+	e.printSeparator()
 }
 
-func (e *Explanations) explainDNS(spec Spec) {
+func (e *Explanations) explainDNS(spec v1beta1.DisruptionSpec) {
 	dns := spec.DNS
 	if dns == nil || len(dns) == 0 {
 		return
@@ -182,62 +110,46 @@ func (e *Explanations) explainDNS(spec Spec) {
 	fmt.Println("💉 injects a dns disruption ...")
 	fmt.Println("\t🥸  to spoof the following hostnames...")
 	for _, data := range dns {
-		fmt.Printf("\t\t👩🏽‍✈️ hostname: %s ...\n", *data.Hostname)
-		fmt.Printf("\t\t\t🧾 has type %s\n", *data.Record.Type)
-		fmt.Printf("\t\t\t🥷🏿  will be spoofed with %s\n", *data.Record.Value)
+		fmt.Printf("\t\t👩🏽‍✈️ hostname: %s ...\n", data.Hostname)
+		fmt.Printf("\t\t\t🧾 has type %s\n", data.Record.Type)
+		fmt.Printf("\t\t\t🥷🏿  will be spoofed with %s\n", data.Record.Value)
 	}
-	e.printSeperator()
+	e.printSeparator()
 }
 
-func (e *Explanations) explainNetworkFailure(spec Spec) {
+func (e *Explanations) explainNetworkFailure(spec v1beta1.DisruptionSpec) {
 	network := spec.Network
 	if network == nil {
 		return
 	}
 	fmt.Println("💉 injects a network disruption ...")
-	if network.Hosts == nil || len(network.Hosts) == 0 {
-		fmt.Println("\t💥 specified no hosts meaning that no filtering on host will be done when applying network failures.")
-	} else {
-		fmt.Printf("\t💥 applies filters so that network failures only apply to outgoing/ingoing traffic from/to the following hosts:\n\t\t🎯 %s\n", strings.Join(network.Hosts, ","))
-	}
+	// TODO rebase onto the new services PR
 
-	if network.Port == nil {
-		fmt.Println("\t💥 specified no port meaning that no filtering on port will be done when applying network failures.")
-	} else {
-		fmt.Printf("\t💥 applies filters so that network failures only apply to port %d\n", *network.Port)
-	}
-
-	if network.Protocol == nil {
-		fmt.Println("\t💥 specifies no Protocol, so by default all protocol are applied (tcp,udp,etc.)")
-	} else {
-		fmt.Printf("\t💥 applies filters so that network failures only apply packets with the %s protocol\n", *network.Protocol)
-	}
-
-	if network.Flow != nil && *network.Flow == "ingress" {
-		fmt.Println("\t💥 applies network failures on incoming traffic to the targets only.")
+	if network.Flow == v1beta1.FlowIngress {
+		fmt.Println("\t💥 applies network failures on incoming traffic to the targets only.") //TODO technically not true, link to docs?
 	} else {
 		fmt.Println("\t💥 applies network failures on outgoing traffic to the targets only.")
 	}
 
-	if network.Drop != nil {
-		fmt.Printf("\t\t💣 applies a packet drop of %d percent.\n", *network.Drop)
+	if network.Drop != 0 {
+		fmt.Printf("\t\t💣 applies a packet drop of %d percent.\n", network.Drop)
 	}
-	if network.Corrupt != nil {
-		fmt.Printf("\t\t💣 will corrupt packets at %d percent.\n", *network.Drop)
+	if network.Corrupt != 0 {
+		fmt.Printf("\t\t💣 will corrupt packets at %d percent.\n", network.Drop)
 	}
-	if network.Delay != nil {
-		fmt.Printf("\t\t💣 applies a packet delay of %d ms.\n", *network.Delay)
-		if network.DelayJitter != nil {
-			fmt.Printf("\t\t\t💣 applies a jitter of %d ms to the delay value to add randomness to the delay.\n", *network.Delay)
+	if network.Delay != 0 {
+		fmt.Printf("\t\t💣 applies a packet delay of %d ms.\n", network.Delay)
+		if network.DelayJitter != 0 {
+			fmt.Printf("\t\t\t💣 applies a jitter of %d ms to the delay value to add randomness to the delay.\n", network.Delay)
 		}
 	}
-	if network.BandwidthLimit != nil {
-		fmt.Printf("\t\t💣 applies a bandwidth limit of %d ms.\n", *network.BandwidthLimit)
+	if network.BandwidthLimit != 0 {
+		fmt.Printf("\t\t💣 applies a bandwidth limit of %d ms.\n", network.BandwidthLimit)
 	}
-	e.printSeperator()
+	e.printSeparator()
 }
 
-func (e *Explanations) explainMultiDisruption(spec Spec) {
+func (e *Explanations) explainMultiDisruption(spec v1beta1.DisruptionSpec) {
 	existsMulti := false
 	if spec.NodeFailure != nil {
 		if spec.CPUPressure != nil || spec.DNS != nil || spec.DiskPressure != nil || spec.Network != nil {
@@ -253,7 +165,7 @@ func (e *Explanations) explainMultiDisruption(spec Spec) {
 		existsMulti = true
 	}
 	if existsMulti {
-		e.printSeperator()
+		e.printSeparator()
 	}
 }
 
@@ -268,30 +180,30 @@ var explainCmd = &cobra.Command{
 }
 
 func explanation(filePath string) {
-	var disruptionSpec DisruptionSpec
+	var disruption v1beta1.Disruption
 	if filePath == "" {
 		fmt.Println(pathError)
 	}
 	fullPath, _ := filepath.Abs(filePath)
-	disruption, err := ioutil.ReadFile(fullPath)
+	disruptionBytes, err := ioutil.ReadFile(fullPath)
 	if err != nil {
 		log.Printf("disruption.Get err   #%v ", err)
 		//panic(err)
 		return
 	}
-	err = yaml.Unmarshal(disruption, &disruptionSpec)
+	err = yaml.Unmarshal(disruptionBytes, &disruption)
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
 	}
 	fmt.Println("This Disruption...")
-	e := Explanations{seperator: "======================================================================================================================================="}
-	e.explainMetaSpec(disruptionSpec.Spec)
-	e.explainMultiDisruption(disruptionSpec.Spec)
-	e.explainNodeFailure(disruptionSpec.Spec)
-	e.explainNetworkFailure(disruptionSpec.Spec)
-	e.explainCPUPressure(disruptionSpec.Spec)
-	e.explainDiskPressure(disruptionSpec.Spec)
-	e.explainDNS(disruptionSpec.Spec)
+	e := Explanations{separator: "======================================================================================================================================="}
+	e.explainMetaSpec(disruption.Spec)
+	e.explainMultiDisruption(disruption.Spec)
+	e.explainNodeFailure(disruption.Spec)
+	e.explainNetworkFailure(disruption.Spec)
+	e.explainCPUPressure(disruption.Spec)
+	e.explainDiskPressure(disruption.Spec)
+	e.explainDNS(disruption.Spec)
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v0.7.3-0.20190327010347-be7ac8be2ae0
 	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/ghodss/yaml v1.0.0
 	github.com/gogo/googleapis v1.4.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/miekg/dns v1.1.31
@@ -42,7 +43,6 @@ require (
 	golang.org/x/net v0.0.0-20200520004742-59133d7f0dd7
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744
 	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/yaml.v2 v2.4.0
 	gotest.tools/v3 v3.0.2 // indirect
 	k8s.io/api v0.18.6
 	k8s.io/apimachinery v0.18.6
