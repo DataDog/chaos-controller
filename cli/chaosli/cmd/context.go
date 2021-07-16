00ff060a8be8c4a3c0c46455a44cc1bbe152bcfe
Azoam <sam.azouzi@datadoghq.com>
2021-07-16 13:37:22 -0500

    fixing linting issues pt1

--- cli/chaosli/cmd/context.go
+++ cli/chaosli/cmd/context.go
@@ -20,6 +20,7 @@ import (
 )
 
 const MAXTARGETSHOW = 10
+const POD = "pod"
 
 func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 	spec := disruption.Spec
@@ -29,6 +30,7 @@ func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 	// do not require namespaces to be described
 	podNamespaces := disruption.ObjectMeta.Namespace
 	rowNames := 1
+
 	if level == "node" {
 		rowNames = 1
 	}
@@ -37,12 +39,14 @@ func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 
 	s := spinner.New(spinner.CharSets[38], 100*time.Millisecond)
 	s.Start()
-	cmd := fmt.Sprintf("kubectl get %s -n %s -l %s | wc -l",level,podNamespaces,labels)
+	cmd := fmt.Sprintf("kubectl get %s -n %s -l %s | wc -l", level, podNamespaces, labels)
 	sizeString, err := exec.Command("bash", "-c", cmd).Output()
+
 	if err != nil {
 		return nil, fmt.Errorf("Could not count the number of pods correlating to target selector: %v", err)
 	}
-	size, err := strconv.Atoi(strings.Trim(string(sizeString),"\n"))
+	size, err := strconv.Atoi(strings.Trim(string(sizeString), "\n"))
+
 	if err != nil {
 		return nil, fmt.Errorf("Could not convert string to integer in context target size: %v", err)
 	}
@@ -51,26 +55,30 @@ func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 	size = size - 1
 	// If the size is 0, first check if changing the level will do anything, otherwise
 	// mention to the user that the labels they are using won't target anything
+
 	if size <= 0 {
 		errorString := fmt.Sprintf("\nThe label selectors chosen (%s) result in 0 targets, meaning this disruption would do nothing given the namespace/cluster/label combination.", labels)
 		newLevel := ""
-		if level == "pod" {
+
+		if level == POD {
 			newLevel = "node"
 		} else {
-			newLevel = "pod"
+			newLevel = POD
 		}
-		cmd := fmt.Sprintf("kubectl get %s -n %s -l %s | wc -l",newLevel,podNamespaces,labels)
+		cmd := fmt.Sprintf("kubectl get %s -n %s -l %s | wc -l", newLevel, podNamespaces, labels)
 		sizeString, err := exec.Command("bash", "-c", cmd).Output()
+
 		if err != nil {
 			return nil, fmt.Errorf("Could not count the number of pods correlating to target selector: %v", err)
 		}
-		size, err = strconv.Atoi(strings.Trim(string(sizeString),"\n"))
+		size, err = strconv.Atoi(strings.Trim(string(sizeString), "\n"))
+
 		if err != nil {
 			return nil, fmt.Errorf("Could not convert string to integer in context target size: %v", err)
 		}
 
 		if size > 0 {
-			errorString = fmt.Sprintf("\nWe noticed that your target size is 0 for level %s given your label selectors. We checked to see if the %s level would give you results and we found %d %ss. Is this the level you wanted to use?",level,newLevel,size,newLevel)
+			errorString = fmt.Sprintf("\nWe noticed that your target size is 0 for level %s given your label selectors. We checked to see if the %s level would give you results and we found %d %ss. Is this the level you wanted to use?", level, newLevel, size, newLevel)
 		}
 		fmt.Println(errorString)
 		return nil, fmt.Errorf("bad sized targets")
@@ -78,8 +86,9 @@ func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 
 	cmd = fmt.Sprintf("kubectl get %s -n %s -l %s | awk '{print $%d}'", level, podNamespaces, labels, rowNames)
 	targets, err := exec.Command("bash", "-c", cmd).Output()
+
 	if err != nil {
-		return nil, fmt.Errorf("Could not grab list of targets names correlating to target selector: %v",err)
+		return nil, fmt.Errorf("Could not grab list of targets names correlating to target selector: %v", err)
 	}
 
 	s.Stop()
@@ -87,29 +96,31 @@ func contextTargetsSize(disruption v1beta1.Disruption) ([]string, error) {
 	targetsShow := []string{}
 	targetsAll := []string{}
 	targetsSplit := strings.Split(string(targets), "\n")
+
 	for i := 0; i < len(targetsSplit); i++ {
 		if len(targetsShow) < MAXTARGETSHOW {
-			targetsShow = append(targetsShow, targetsSplit[i] )
+			targetsShow = append(targetsShow, targetsSplit[i])
 		}
 		if targetsSplit[i] == "NAME" || targetsSplit[i] == "" {
 			continue
 		}
-		targetsAll = append(targetsAll, targetsSplit[i] )
+		targetsAll = append(targetsAll, targetsSplit[i])
 	}
-
-
 
 	fmt.Printf("\n🎯 There are %d %ss that will be targeted\n", size, level)
 	fmt.Println("📝 Here are a small set of those targets:")
+
 	for _, target := range targetsShow {
 		fmt.Println(target)
 	}
+
 	if size >= MAXTARGETSHOW {
 		fmt.Println("...")
 	}
+
 	PrintSeparator()
 
-	return targetsAll,nil
+	return targetsAll, nil
 }
 
 func grabDataForTargets(targets []string, disruption v1beta1.Disruption) ([]v1.Pod, []v1.Node, error) {
@@ -120,46 +131,51 @@ func grabDataForTargets(targets []string, disruption v1beta1.Disruption) ([]v1.P
 
 	s := spinner.New(spinner.CharSets[38], 100*time.Millisecond)
 	s.Start()
+
 	for _, targetName := range targets {
 		pod := v1.Pod{}
 		node := v1.Node{}
 
-		cmd := fmt.Sprintf("kubectl get %s -o json -n %s %s",level,namespace,targetName)
+		cmd := fmt.Sprintf("kubectl get %s -o json -n %s %s", level, namespace, targetName)
 		targetData, err := exec.Command("bash", "-c", cmd).Output()
+
 		if err != nil {
-			return nil,nil, fmt.Errorf("Could not grab target data: %v",err)
+			return nil, nil, fmt.Errorf("Could not grab target data: %v", err)
 		}
 
-		if level == "pod"{
-			if err := json.Unmarshal([]byte(targetData), &pod); err != nil {
-				return nil,nil, fmt.Errorf("Json encoding failed: %v", err)
+		if level == POD {
+			if err := json.Unmarshal(targetData, &pod); err != nil {
+				return nil, nil, fmt.Errorf("Json encoding failed: %v", err)
 			}
 			pods = append(pods, pod)
 		} else {
-			if err := json.Unmarshal([]byte(targetData), &node); err != nil {
-				return nil,nil, fmt.Errorf("Json encoding failed: %v", err)
+			if err := json.Unmarshal(targetData, &node); err != nil {
+				return nil, nil, fmt.Errorf("Json encoding failed: %v", err)
 			}
-			nodes = append(nodes,node)
+			nodes = append(nodes, node)
 		}
 	}
 	s.Stop()
 
-	return pods,nodes, nil
+	return pods, nodes, nil
 }
 
 func printContainerStatus(targetInfo []v1.Pod) {
 	percentCollect := make(map[string]float64)
 
-	fmt.Println("\nLets take a look at the status of your Targeted Pod Containers...")
+	fmt.Println("\nLets take" +
+		" a look at the status of your Targeted Pod Containers...")
 	totalContainers := 0
 	for i := 0; i < len(targetInfo); i++ {
 		pod := targetInfo[i]
-		info := "\t🥸  Pod Name: "+pod.Name+"\n"
+		info := "\t🥸  Pod Name: " + pod.Name + "\n"
+
 		for j := 0; j < len(pod.Status.ContainerStatuses); j++ {
 			totalContainers += 1
 			containerStatus := pod.Status.ContainerStatuses[j]
-			info += "\t\t🤓 Container Name: "+containerStatus.Name+"\n"+
-					"\t\t⭕️ Ready Status: "+strconv.FormatBool(containerStatus.Ready)+"\n"
+			info += "\t\t🤓 Container Name: " + containerStatus.Name + "\n" +
+				"\t\t⭕️ Ready Status: " + strconv.FormatBool(containerStatus.Ready) + "\n"
+
 			if containerStatus.State.Running != nil {
 				info += "\t\t📝 State: Running\n\n"
 				percentCollect["Running"] += 1.0
@@ -177,23 +193,25 @@ func printContainerStatus(targetInfo []v1.Pod) {
 		}
 
 		if i < MAXTARGETSHOW {
-			fmt.Printf(info+"\n\n")
+			fmt.Printf(info + "\n\n")
 		}
 	}
 
 	PrintSeparator()
 	percentInfo := "Lets look at the overall status of your targeted pod's containers...\n"
-	for key,value := range percentCollect {
-		roundedValue := (value/float64(totalContainers))*100.00
+
+	for key, value := range percentCollect {
+		roundedValue := (value / float64(totalContainers)) * 100.00
+
 		if key == "Ready" {
-			percentInfo += "\tState:                 "+"Ready"+"\n"+
-				           "\tPercent:               "+fmt.Sprint(math.Round(roundedValue*100)/100)+"%\n\n"
-			percentInfo += "\tState:                 "+"Not Ready"+"\n"+
-				           "\tPercent:               "+fmt.Sprint(100.00-math.Round(roundedValue*100)/100)+"%\n\n"
+			percentInfo += "\tState:                 " + "Ready" + "\n" +
+				"\tPercent:               " + fmt.Sprint(math.Round(roundedValue*100)/100) + "%\n\n"
+			percentInfo += "\tState:                 " + "Not Ready" + "\n" +
+				"\tPercent:               " + fmt.Sprint(100.00-math.Round(roundedValue*100)/100) + "%\n\n"
 			continue
 		}
-		percentInfo += "\tState:                 "+key+"\n"+
-			"\tPercent:               "+fmt.Sprint(math.Round(roundedValue*100)/100)+"%\n\n"
+		percentInfo += "\tState:                 " + key + "\n" +
+			"\tPercent:               " + fmt.Sprint(math.Round(roundedValue*100)/100) + "%\n\n"
 	}
 	fmt.Println(percentInfo)
 }
@@ -203,38 +221,43 @@ func printPodStatus(targetsInfo []v1.Pod) {
 	percentCollectCondition := make(map[string]float64)
 
 	fmt.Println("\nLets take a look at the status of your Targeted Pods...")
-	for i := 0;  i < len(targetsInfo); i++ {
+
+	for i := 0; i < len(targetsInfo); i++ {
 		pod := targetsInfo[i]
-		info := "\t🥸  Pod Name: "+pod.Name+"\n"+
-				"\t👵🏾 Pod Host IP: "+pod.Status.HostIP+"\n"+
-				"\t👧🏾 Pod IP: "+pod.Status.PodIP+"\n"+
-				"\t🌒 Pod Phase: "+string(pod.Status.Phase)+"\n"+
-				"\t📜 Pod Conditions:\n"
+		info := "\t🥸  Pod Name: " + pod.Name + "\n" +
+			"\t👵🏾 Pod Host IP: " + pod.Status.HostIP + "\n" +
+			"\t👧🏾 Pod IP: " + pod.Status.PodIP + "\n" +
+			"\t🌒 Pod Phase: " + string(pod.Status.Phase) + "\n" +
+			"\t📜 Pod Conditions:\n"
+
 		for _, status := range pod.Status.Conditions {
 			info = info +
-				"\t\t⭕️ Type: "+string(status.Type)+"\n"+
-				"\t\tℹ️  Status: "+string(status.Status)+"\n\n"
-			percentCollectCondition[string(status.Type)+"/"+string(status.Status)] += 1.0/float64(len(targetsInfo))
+				"\t\t⭕️ Type: " + string(status.Type) + "\n" +
+				"\t\tℹ️  Status: " + string(status.Status) + "\n\n"
+			percentCollectCondition[string(status.Type)+"/"+string(status.Status)] += 1.0 / float64(len(targetsInfo))
 		}
-		percentCollectPhase[string(pod.Status.Phase)] += 1.0/float64(len(targetsInfo))
+		percentCollectPhase[string(pod.Status.Phase)] += 1.0 / float64(len(targetsInfo))
+
 		if i < MAXTARGETSHOW {
-			fmt.Printf(info+"\n\n")
+			fmt.Printf(info + "\n\n")
 		}
 	}
 	PrintSeparator()
 	percentInfo := "Lets look at the overall status...\n"
-	for key,value := range percentCollectPhase {
-		roundedValue := value*100.00
-		percentInfo += "\tPhase:                 "+key+"\n"+
-			 		   "\tPercent:               "+fmt.Sprint(math.Round(roundedValue*100)/100)+"%\n\n"
+
+	for key, value := range percentCollectPhase {
+		roundedValue := value * 100.00
+		percentInfo += "\tPhase:                 " + key + "\n" +
+			"\tPercent:               " + fmt.Sprint(math.Round(roundedValue*100)/100) + "%\n\n"
 	}
 
-	for key,value := range percentCollectCondition {
-		roundedValue := value*100.00
-		percentInfo += "\tCondition Type/Status: "+key+"\n"+
-			           "\tPercent:               "+fmt.Sprint(math.Round(roundedValue*100)/100)+"%\n\n"
+	for key, value := range percentCollectCondition {
+		roundedValue := value * 100.00
+		percentInfo += "\tCondition Type/Status: " + key + "\n" +
+			"\tPercent:               " + fmt.Sprint(math.Round(roundedValue*100)/100) + "%\n\n"
 	}
 	fmt.Println(percentInfo)
+
 	PrintSeparator()
 }
 
@@ -242,39 +265,45 @@ func printNodeStatus(targetsInfo []v1.Node) {
 	percentCollectCondition := make(map[string]float64)
 
 	fmt.Println("\nLets take a look at the status of your Targeted Nodes...")
-	for i := 0;  i < len(targetsInfo); i++ {
+
+	for i := 0; i < len(targetsInfo); i++ {
 		node := targetsInfo[i]
-		info := "\t🥸  Node Name: "+node.Name+"\n"+
+		info := "\t🥸  Node Name: " + node.Name + "\n" +
 			"\t📲 Node Addresses: \n"
+
 		for _, address := range node.Status.Addresses {
-			info += "\t\t⭕️ Type: "+string(address.Type)+"\n"+
-					"\t\tℹ️  Address: "+address.Address+"\n"
+			info += "\t\t⭕️ Type: " + string(address.Type) + "\n" +
+				"\t\tℹ️  Address: " + address.Address + "\n"
 		}
-		info +=	"\t📜 Node Conditions:\n"
+		info += "\t📜 Node Conditions:\n"
+
 		for _, status := range node.Status.Conditions {
 			info = info +
-				"\t\t⭕️ Type: "+string(status.Type)+"\n"+
-				"\t\tℹ️  Status: "+string(status.Status)+"\n"+
-				"\t\t🤨 Reason: "+status.Reason+"\n\n"
-			percentCollectCondition[string(status.Type)+"/"+string(status.Status)] += 1.0/float64(len(targetsInfo))
+				"\t\t⭕️ Type: " + string(status.Type) + "\n" +
+				"\t\tℹ️  Status: " + string(status.Status) + "\n" +
+				"\t\t🤨 Reason: " + status.Reason + "\n\n"
+			percentCollectCondition[string(status.Type)+"/"+string(status.Status)] += 1.0 / float64(len(targetsInfo))
 		}
+
 		if i < MAXTARGETSHOW {
-			fmt.Printf(info+"\n\n")
+			fmt.Printf(info + "\n\n")
 		}
 	}
+
 	PrintSeparator()
+
 	percentInfo := "Lets look at the overall status...\n"
-	for key,value := range percentCollectCondition {
-		roundedValue := value*100.00
-		percentInfo += "\tCondition Type/Status: "+key+"\n"+
-			"\tPercent:               "+fmt.Sprint(math.Round(roundedValue*100)/100)+"%\n\n"
+	for key, value := range percentCollectCondition {
+		roundedValue := value * 100.00
+		percentInfo += "\tCondition Type/Status: " + key + "\n" +
+			"\tPercent:               " + fmt.Sprint(math.Round(roundedValue*100)/100) + "%\n\n"
 	}
 
 	fmt.Println(percentInfo)
 	PrintSeparator()
 }
 
-func checkKubectl(spec v1beta1.DisruptionSpec) error {
+func checkKubectl() error {
 	cmd := exec.Command("kubectl", "get", "pods", "-n", "chaos-engineering")
 	_, err := cmd.Output()
 
@@ -285,7 +314,7 @@ func checkKubectl(spec v1beta1.DisruptionSpec) error {
 	return nil
 }
 
-var contextCmd = &cobra.Command {
+var contextCmd = &cobra.Command{
 	Use:   "context",
 	Short: "contextualizes disruption config",
 	Long:  `makes use of kubectl to give a better idea of how big the scope of the disruption will be.`,
@@ -333,24 +362,24 @@ func contextualize(path string) {
 		log.Fatalf("There were some problems when validating your disruption: %v", err)
 	}
 
-	err = checkKubectl(disruption.Spec)
+	err = checkKubectl()
 	if err != nil {
 		log.Fatalf("Could not find/use kubectl command, make sure it is in your PATH variable and that all authorizations for the command are set (login to your authorization provider (e.g. AppGate).")
 	}
 
 	targets, err := contextTargetsSize(disruption)
 	if err != nil {
-		log.Fatalf("Could not grab context regarding size and names of pods: %v",err)
+		log.Fatalf("Could not grab context regarding size and names of pods: %v", err)
 	}
 
 	// Because the state of each of the pods and the state of their containers uses the same information, lets grab
 	// that set of data here and then pass them to other functions to do the searching
-	podsData, nodesData, err := grabDataForTargets(targets,disruption)
+	podsData, nodesData, err := grabDataForTargets(targets, disruption)
 	if err != nil {
-		log.Fatalf("Attempted to grab data for targets and failed: %v",err)
+		log.Fatalf("Attempted to grab data for targets and failed: %v", err)
 	}
 
-	if disruption.Spec.Level == "pod" {
+	if disruption.Spec.Level == POD {
 		printPodStatus(podsData)
 		printContainerStatus(podsData)
 	} else {
