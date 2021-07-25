a5b32d41da8c20fb241b2cdb760091f1b6871d37
Blaze Bissar <blazejbissar@gmail.com>
2021-07-25 17:42:04 -0400

    Add http disruption Validate method

--- api/v1beta1/http_disruption.go
+++ api/v1beta1/http_disruption.go
@@ -6,7 +6,7 @@
 package v1beta1
 
 import (
-//	"errors"
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -18,7 +18,7 @@ type HTTPDisruptionSpec []TargetDomain
 type TargetDomain struct {
 	Domain string         `json:"domain"`
 	Header []RequestField `json:"header"`
-	Ports PortTypes `json:"ports"`
+	Ports  PortTypes      `json:"ports"`
 }
 
 // RequestFields represents some of the fields present in the http request header
@@ -30,7 +30,7 @@ type RequestField struct {
 // PortTypes represents the HTTPS and HTTP ports to forward via iptables and the ones that the corresponding proxy services should listen on
 type PortTypes struct {
 	HTTPS []int `json:"https"`
-	HTTP []int `json:"http"`
+	HTTP  []int `json:"http"`
 }
 
 // GenerateArgs generates injection pod arguments for the given spec
@@ -47,12 +47,12 @@ func (s HTTPDisruptionSpec) GenerateArgs() []string {
 	for _, target := range s {
 		whiteSpaceCleanedDomain := strings.ReplaceAll(target.Domain, " ", "")
 
-		for _, header := range(target.Header) {
+		for _, header := range target.Header {
 			whiteSpaceCleanedUri := strings.ReplaceAll(header.Uri, " ", "")
 			whiteSpaceCleanedMethod := strings.ReplaceAll(header.Method, " ", "")
 			arg := fmt.Sprintf("--request-field %s%s;%s", whiteSpaceCleanedDomain, whiteSpaceCleanedUri, whiteSpaceCleanedMethod)
 			targetDomainArgs = append(targetDomainArgs, arg)
-			}
+		}
 
 		for _, port := range target.Ports.HTTP {
 			httpPortArg = fmt.Sprintf("%s,%d", httpPortArg, port)
@@ -72,8 +72,34 @@ func (s HTTPDisruptionSpec) GenerateArgs() []string {
 	return args
 }
 
-// Validate validates that there are no missing URIs or methods in the given http disruption spec
+// Validate validates that there are no missing domains, ports, URIs or methods in the given http disruption spec
 func (s HTTPDisruptionSpec) Validate() error {
-	// TODO: implement
+	for _, target := range s {
+		if target.Domain == "" {
+			return errors.New("No domain specified in the http disruption")
+		}
+
+		if len(target.Header) == 0 {
+			return errors.New("No header values specified in the http disruption")
+		}
+
+		for _, header := range target.Header {
+			if header.Uri == "" {
+				return errors.New("Header is missing a uri value in http disruption")
+			}
+			if header.Method == "" {
+				return errors.New("Header is missing an http method value in http disruption")
+			}
+		}
+
+		if len(target.Ports.HTTP) == 0 {
+			return errors.New("Missing http ports in the http disruption")
+		}
+
+		if len(target.Ports.HTTPS) == 0 {
+			return errors.New("Missing https ports in the http disruption")
+		}
+	}
+
 	return nil
 }
