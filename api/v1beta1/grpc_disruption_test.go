7e17f0b11e122ae6e8677d648dce48dd9248e744
Tay Nishimura <tay.nishimura@datadoghq.com>
2021-09-16 19:16:05 -0400

    typo

--- api/v1beta1/grpc_disruption_test.go
+++ api/v1beta1/grpc_disruption_test.go
@@ -3,21 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021 Datadog, Inc.
 
-/*
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1beta1_test
 
 import (
@@ -109,7 +94,7 @@ var _ = Describe("QueryPercent Validation", func() {
 				},
 				{
 					TargetEndpoint:   "/chaos_dogfood.ChaosDogfood/order",
-					ErrorToReturn:    "CANCELED",
+					ErrorToReturn:    "ALREADY_EXISTS",
 					OverrideToReturn: "{}",
 					QueryPercent:     40,
 				},
@@ -130,7 +115,7 @@ var _ = Describe("QueryPercent Validation", func() {
 				},
 				{
 					TargetEndpoint:   "/chaos_dogfood.ChaosDogfood/order",
-					ErrorToReturn:    "CANCELED",
+					ErrorToReturn:    "ALREADY_EXISTS",
 					OverrideToReturn: "{}",
 					QueryPercent:     49,
 				},
@@ -139,7 +124,7 @@ var _ = Describe("QueryPercent Validation", func() {
 		Expect(spec.Validate()).To(BeNil())
 	})
 
-	It("Query percent which sums to more than 100 on two endpoints does validate", func() {
+	It("Query percent which sums to more than 100 on two endpoints does not validate", func() {
 		spec := v1beta1.GRPCDisruptionSpec{
 			Port: 50051,
 			Endpoints: []v1beta1.EndpointAlteration{
@@ -151,7 +136,7 @@ var _ = Describe("QueryPercent Validation", func() {
 				},
 				{
 					TargetEndpoint:   "/chaos_dogfood.ChaosDogfood/order",
-					ErrorToReturn:    "CANCELED",
+					ErrorToReturn:    "ALREADY_EXISTS",
 					OverrideToReturn: "{}",
 					QueryPercent:     51,
 				},
