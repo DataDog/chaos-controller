ae86e02691cad0d99689aa91f46d56e865a3d37f
Nathan Tournant <nathan.tournant@datadoghq.com>
2021-10-26 16:29:28 +0200

    feat: manage empty version tag

--- cli/chaosli/cmd/version.go
+++ cli/chaosli/cmd/version.go
@@ -6,13 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "local-dev"
+var Version string
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "display chaosli version",
 	Long:  `shows the currently used version of the chaosli - upgrade with brew if necessary`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if Version == "" {
+			Version = "version unspecified"
+		}
 		fmt.Println("chaosli", Version)
 	},
 }
