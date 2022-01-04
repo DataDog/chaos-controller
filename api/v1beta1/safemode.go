42b340bd1c9405395a08ce4a8f6508b5fc8409d3
Azoam <sam.azouzi@datadoghq.com>
2022-01-04 13:20:55 -0500

    update manifests and gofmt

--- api/v1beta1/safemode.go
+++ api/v1beta1/safemode.go
@@ -11,7 +11,7 @@ package v1beta1
 // If IgnoreAll is true by default, the user can change its value
 type SafemodeSpec struct {
 	IgnoreAll                 bool `json:"ignoreAll,omitempty"`
-	IgnoreCountNotTooLarge        bool `json:"ignoreCountNotToLarge,omitempty"`
+	IgnoreCountNotTooLarge    bool `json:"ignoreCountNotToLarge,omitempty"`
 	IgnoreNoPortOrHost        bool `json:"ignoreNoPortOrHost,omitempty"`
 	IgnoreSporadicTargets     bool `json:"ignoreSporadicTargets,omitempty"`
 	IgnoreSpecificContainDisk bool `json:"ignoreSpecificContainDisk,omitempty"`
