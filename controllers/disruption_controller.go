932e494a494394f417b610b7ec71fa8594bafc6a
Philip Thompson <philip.thompson@datadoghq.com>
2022-02-07 12:57:22 -0500

    No need to put event on expired chaos pods now that they self-terminate

--- controllers/disruption_controller.go
+++ controllers/disruption_controller.go
@@ -191,17 +191,6 @@ func (r *DisruptionReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 			r.Recorder.Event(instance, "Normal", "DurationOver", fmt.Sprintf("The disruption has lived longer than its specified duration, and will be garbage collected after %s.", r.ExpiredDisruptionGCDelay))
 
-			chaosPods, err := r.getChaosPods(instance, nil)
-			if err != nil {
-				r.log.Errorw("couldn't list instance's chaos pods", "err", err)
-
-				return ctrl.Result{}, err
-			}
-
-			for _, pod := range chaosPods {
-				r.Recorder.Event(&pod, "Normal", "DurationOver", fmt.Sprintf("The chaos pod has lived longer than the disruption duration, and will be garbage collected after %s.", r.ExpiredDisruptionGCDelay))
-			}
-
 			requeueDelay := r.ExpiredDisruptionGCDelay
 
 			r.log.Infow("requeuing disruption to check for its expiration", "requeueDelay", requeueDelay.String())
