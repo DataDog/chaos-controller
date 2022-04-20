0d63e50b095434820528e582d86629f70183ade0
Azouzi <sam.azouzi@datadoghq.com>
2022-04-20 09:22:01 -0400

    Update api/v1beta1/disruption_webhook.go
    Co-authored-by: Philip Thompson <philip.thompson@datadoghq.com>

--- api/v1beta1/disruption_webhook.go
+++ api/v1beta1/disruption_webhook.go
@@ -269,7 +269,7 @@ func safetyNetCountNotTooLarge(r Disruption) (bool, string, error) {
 	namespaceThreshold := 0.8
 	clusterThreshold := 0.66
 
-	if r.Spec.Unsafemode == nil {
+	if r.Spec.Unsafemode != nil {
 		if r.Spec.Unsafemode.Config != nil && r.Spec.Unsafemode.Config.CountTooLarge != nil {
 			if r.Spec.Unsafemode.Config.CountTooLarge.NamespaceThreshold != 0 {
 				namespaceThreshold = float64(r.Spec.Unsafemode.Config.CountTooLarge.NamespaceThreshold) / 100.0
