6f9f202bfc30cf5675d71d747b247c0a4558a391
Nathan Tournant <nathan.tournant@datadoghq.com>
2022-08-10 15:23:22 +0200

    fix: e2e testing fixed

--- controllers/disruption_controller_test.go
+++ controllers/disruption_controller_test.go
@@ -357,10 +357,10 @@ var _ = Describe("Disruption Controller", func() {
 
 		It("should target all the selected pods", func() {
 			By("Ensuring that the inject pod has been created")
-			Eventually(func() error { return expectChaosPod(disruption, 6) }, timeout).Should(Succeed())
+			Eventually(func() error { return expectChaosPod(disruption, 4) }, timeout).Should(Succeed())
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			Expect(expectChaosInjectors(disruption, 6)).To(BeNil())
+			Expect(expectChaosInjectors(disruption, 4)).To(BeNil())
 		})
 	})
 
@@ -372,10 +372,10 @@ var _ = Describe("Disruption Controller", func() {
 
 		It("should target all the selected pods", func() {
 			By("Ensuring that the chaos pods have been created")
-			Eventually(func() error { return expectChaosPod(disruption, 12) }, timeout).Should(Succeed())
+			Eventually(func() error { return expectChaosPod(disruption, 8) }, timeout).Should(Succeed())
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			Expect(expectChaosInjectors(disruption, 18)).To(BeNil())
+			Expect(expectChaosInjectors(disruption, 12)).To(BeNil())
 		})
 	})
 
