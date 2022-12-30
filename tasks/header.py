76400e35a7ef1501409f72cc72a2a9c36ad6d0d3
Philip Thompson <philip.thompson@datadoghq.com>
2022-12-30 09:19:36 -0500

    Update header.py

--- tasks/header.py
+++ tasks/header.py
@@ -43,7 +43,7 @@ def generate_header_with_tag(tag: str) -> t.List:
         f"{tag} Unless explicitly stated otherwise all files in this repository are licensed\n",
         f"{tag} under the Apache License Version 2.0.\n",
         f"{tag} This product includes software developed at Datadog (https://www.datadoghq.com/).\n",
-        f"{tag} Copyright 2022 Datadog, Inc.\n",
+        f"{tag} Copyright 2023 Datadog, Inc.\n",
     ]
 
 
