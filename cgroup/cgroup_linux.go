9313434bddd0e374b0b9e8e9f854564be8478f41
taihuynh167 <113124765+taihuynh167@users.noreply.github.com>
2023-02-01 10:20:36 +0000

    Remove unused attributes and methods. (#650)

--- cgroup/cgroup_linux.go
+++ cgroup/cgroup_linux.go
@@ -7,12 +7,10 @@ package cgroup
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 
-	"github.com/DataDog/chaos-controller/env"
 	"github.com/opencontainers/runc/libcontainer/cgroups"
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs"
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs2"
@@ -20,19 +18,11 @@ import (
 	"go.uber.org/zap"
 )
 
-func parse(cgroupFile string) (map[string]string, error) {
-	return cgroups.ParseCgroupFile(cgroupFile)
-}
-
-func pathExists(path string) (bool, error) {
-	return cgroups.PathExists(path), nil
-}
-
 func cgroupManager(cgroupFile string) (cgroups.Manager, error) {
 	cg := &configs.Cgroup{
 		Resources: &configs.Resources{},
 	}
-	cgroupPaths, err := parse(cgroupFile)
+	cgroupPaths, err := cgroups.ParseCgroupFile(cgroupFile)
 
 	if err != nil {
 		return nil, err
@@ -61,14 +51,12 @@ func cgroupManager(cgroupFile string) (cgroups.Manager, error) {
 type cgroup struct {
 	manager *cgroups.Manager
 	dryRun  bool
-	paths   map[string]string
-	mount   string
 	log     *zap.SugaredLogger
 }
 
 // Read reads the given cgroup file data and returns the content as a string
-func (m cgroup) Read(controller, file string) (string, error) {
-	manager := *m.manager
+func (cg cgroup) Read(controller, file string) (string, error) {
+	manager := *cg.manager
 	controllerDir := manager.Path(controller)
 	content, err := cgroups.ReadFile(controllerDir, file)
 
@@ -80,16 +68,16 @@ func (m cgroup) Read(controller, file string) (string, error) {
 }
 
 // Write writes the given data to the given cgroup kind
-func (m cgroup) Write(controller, file, data string) error {
-	manager := *m.manager
+func (cg cgroup) Write(controller, file, data string) error {
+	manager := *cg.manager
 	controllerDir := manager.Path(controller)
 
 	return cgroups.WriteFile(controllerDir, file, data)
 }
 
 // Exists returns true if the given cgroup exists, false otherwise
-func (m cgroup) Exists(controller string) bool {
-	manager := *m.manager
+func (cg cgroup) Exists(controller string) bool {
+	manager := *cg.manager
 	controllerDir := manager.Path(controller)
 
 	return cgroups.PathExists(fmt.Sprintf("%s/cgroup.procs", controllerDir))
@@ -98,22 +86,22 @@ func (m cgroup) Exists(controller string) bool {
 // Join adds the given PID to the given cgroup
 // If inherit is set to true, all PID of the same group will be moved to the cgroup (writing to cgroup.procs file)
 // Otherwise, only the given PID will be moved to the cgroup (writing to tasks file)
-func (m cgroup) Join(controller string, pid int, inherit bool) error {
+func (cg cgroup) Join(controller string, pid int, inherit bool) error {
 	file := "tasks"
 
 	if inherit {
 		file = "cgroup.procs"
 	}
 
-	manager := *m.manager
+	manager := *cg.manager
 	controllerDir := manager.Path(controller)
 
 	return cgroups.WriteFile(controllerDir, file, strconv.Itoa(pid))
 }
 
 // DiskThrottleRead adds a disk throttle on read operations to the given disk identifier
-func (m cgroup) DiskThrottleRead(identifier, bps int) error {
-	manager := *m.manager
+func (cg cgroup) DiskThrottleRead(identifier, bps int) error {
+	manager := *cg.manager
 	controllerDir := manager.Path("blkio")
 	file := "blkio.throttle.read_bps_device"
 	data := fmt.Sprintf("%d:0 %d", identifier, bps)
@@ -122,8 +110,8 @@ func (m cgroup) DiskThrottleRead(identifier, bps int) error {
 }
 
 // DiskThrottleWrite adds a disk throttle on write operations to the given disk identifier
-func (m cgroup) DiskThrottleWrite(identifier, bps int) error {
-	manager := *m.manager
+func (cg cgroup) DiskThrottleWrite(identifier, bps int) error {
+	manager := *cg.manager
 	controllerDir := manager.Path("blkio")
 	file := "blkio.throttle.write_bps_device"
 	data := fmt.Sprintf("%d:0 %d", identifier, bps)
@@ -131,33 +119,18 @@ func (m cgroup) DiskThrottleWrite(identifier, bps int) error {
 	return cgroups.WriteFile(controllerDir, file, data)
 }
 
-func (m cgroup) IsCgroupV2() bool {
+func (cg cgroup) IsCgroupV2() bool {
 	return false
 }
 
 // NewManager creates a new cgroup manager from the given cgroup root path
 func NewManager(dryRun bool, pid uint32, log *zap.SugaredLogger) (Manager, error) {
-	mount, ok := os.LookupEnv(env.InjectorMountCgroup)
-	if !ok {
-		return nil, fmt.Errorf("environment variable %s doesn't exist", env.InjectorMountCgroup)
-	}
-
-	// create cgroups manager
-	cgroupPaths, err := parse(fmt.Sprintf("/proc/%d/cgroup", pid))
-	if err != nil {
-		return nil, err
-	}
-
-	isCgroupV2, err := pathExists("/sys/fs/cgroup/cgroup.controllers")
-	if err != nil {
-		return nil, err
-	}
-
 	manager, err := cgroupManager(fmt.Sprintf("/proc/%d/cgroup", pid))
 	if err != nil {
 		return nil, err
 	}
 
+	isCgroupV2 := cgroups.PathExists("/sys/fs/cgroup/cgroup.controllers")
 	if isCgroupV2 {
 		return cgroupV2{
 			manager: &manager,
@@ -168,8 +141,6 @@ func NewManager(dryRun bool, pid uint32, log *zap.SugaredLogger) (Manager, error
 	return cgroup{
 		manager: &manager,
 		dryRun:  dryRun,
-		paths:   cgroupPaths,
-		mount:   mount,
 		log:     log,
 	}, nil
 }
