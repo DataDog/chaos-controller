b6abcc312fcd3554a6df9649f081230b122c97a4
Philip Thompson <philip.thompson@datadoghq.com>
2023-02-08 14:48:19 -0500

    Read new flag in the injector

--- cli/injector/main.go
+++ cli/injector/main.go
@@ -65,6 +65,7 @@ var (
 	targetName           string
 	targetNodeName       string
 	onInit               bool
+	pulseInitialDelay    time.Duration
 	pulseActiveDuration  time.Duration
 	pulseDormantDuration time.Duration
 	deadlineRaw          string
@@ -94,6 +95,7 @@ func init() {
 	rootCmd.PersistentFlags().StringSliceVar(&rawTargetContainers, "target-containers", []string{}, "Targeted containers")
 	rootCmd.PersistentFlags().StringVar(&targetPodIP, "target-pod-ip", "", "Pod IP of targeted pod")
 	rootCmd.PersistentFlags().BoolVar(&onInit, "on-init", false, "Apply the disruption on initialization, requiring a synchronization with the chaos-handler container")
+	rootCmd.PersistentFlags().DurationVar(&pulseInitialDelay, "pulse-initial-delay", time.Duration(0), "Duration to wait after injector starts before beginning the activeDuration")
 	rootCmd.PersistentFlags().DurationVar(&pulseActiveDuration, "pulse-active-duration", time.Duration(0), "Duration of the disruption being active in a pulsing disruption (empty if the disruption is not pulsing)")
 	rootCmd.PersistentFlags().DurationVar(&pulseDormantDuration, "pulse-dormant-duration", time.Duration(0), "Duration of the disruption being dormant in a pulsing disruption (empty if the disruption is not pulsing)")
 	rootCmd.PersistentFlags().StringVar(&deadlineRaw, "deadline", "", "Timestamp at which the disruption must be over by")
