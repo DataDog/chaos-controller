b545529912b8ef0c39bb87e5ca0945663baf074f
Philip Thompson <philip.thompson@datadoghq.com>
2023-05-17 14:29:19 -0400

    make lint

--- controllers/disruption_controller.go
+++ controllers/disruption_controller.go
@@ -263,7 +263,7 @@ func (r *DisruptionReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		// check if we have reached trigger.createPods. If not, skip the rest of reconciliation.
 		requeueAfter := time.Until(TimeToCreatePods(instance.Spec.Triggers, instance.CreationTimestamp.Time))
 		if requeueAfter > (time.Second * 5) {
-			requeueAfter = requeueAfter - (time.Second * 5)
+			requeueAfter -= (time.Second * 5)
 			r.log.Debugw("requeuing disruption as we haven't yet reached trigger.createPods", "requeueAfter", requeueAfter.String())
 
 			return ctrl.Result{RequeueAfter: requeueAfter}, nil
