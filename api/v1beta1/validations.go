b56b69a8c2c79bd206d248c835fcbc224fb071a3
Azouzi <sam.azouzi@datadoghq.com>
2023-05-30 13:21:13 -0400

    [398] Better Validation Logging for Improper Service Targeting Config (#718)
    * add catch for when a user adds to services target, but improperly sets values
    
    * linting issues 1
    
    * linting issues 2

--- api/v1beta1/validations.go
+++ api/v1beta1/validations.go
@@ -29,6 +29,10 @@ func validateServices(k8sClient client.Client, services []NetworkDisruptionServi
 		// try to get the service and throw an error if it does not exist
 		if err := k8sClient.Get(context.Background(), serviceKey, &k8sService); err != nil {
 			if client.IgnoreNotFound(err) == nil {
+				if service.Namespace == "" || service.Name == "" {
+					return fmt.Errorf("either service namespace or name have not been properly set for this service: %s/%s -> namespace/name", service.Namespace, service.Name)
+				}
+
 				return fmt.Errorf("the service specified in the network disruption (%s/%s) does not exist", service.Namespace, service.Name)
 			}
 
