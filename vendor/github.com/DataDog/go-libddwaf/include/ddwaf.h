fd37c5a867126b160868f7e9decb98554b1f5afe
luphaz <luphaz@users.noreply.github.com>
2023-06-23 11:06:50 +0200

    feat(datadog): install dd agent in local cluster

--- vendor/github.com/DataDog/go-libddwaf/include/ddwaf.h
+++ vendor/github.com/DataDog/go-libddwaf/include/ddwaf.h
@@ -10,10 +10,10 @@
 #ifdef __cplusplus
 namespace ddwaf{
 class waf;
-class context;
+class context_wrapper;
 } // namespace ddwaf
 using ddwaf_handle = ddwaf::waf *;
-using ddwaf_context = ddwaf::context *;
+using ddwaf_context = ddwaf::context_wrapper *;
 
 extern "C"
 {
@@ -226,8 +226,7 @@ ddwaf_handle ddwaf_init(const ddwaf_object *ruleset,
  * @param rule ddwaf::object map containing rules, exclusions, rules_override and rules_data. (nonnull)
  * @param info Optional ruleset parsing diagnostics. (nullable)
  *
- * @return Handle to the new WAF instance or NULL if there were no new updates
- *         or there was an error processing the ruleset.
+ * @return Handle to the new WAF instance or NULL if there was an error processing the ruleset.
  **/
 ddwaf_handle ddwaf_update(ddwaf_handle handle, const ddwaf_object *ruleset,
     ddwaf_ruleset_info *info);
