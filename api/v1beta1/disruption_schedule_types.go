428c070658fe08fe5a78f2fa14168f7dcfac1236
Diyara Bissembayeva <diyara.bissembayeva@datadoghq.com>
2023-07-17 20:41:38 -0400

    Implement required field validations in DisruptionScheduleSpec

--- api/v1beta1/disruption_schedule_types.go
+++ api/v1beta1/disruption_schedule_types.go
@@ -33,14 +33,20 @@ type DisruptionScheduleList struct {
 }
 
 type DisruptionScheduleSpec struct {
-	//+kubebuilder:validation:MinLength=0
+	// +kubebuilder:validation:MinLength=0
+	// +kubebuilder:validation:Required
+	// +ddmark:validation:Required=true
 	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
 	Schedule string `json:"schedule"`
 
+	// +kubebuilder:validation:Required
+	// +ddmark:validation:Required=true
 	// TargetResource specifies the resource to run disruptions against.
 	// It can only be a deployment or statefulset.
 	TargetResource TargetResourceSpec `json:"targetResource"`
 
+	// +kubebuilder:validation:Required
+	// +ddmark:validation:Required=true
 	// Specifies the Disruption that will be created when executing a DisruptionShedule.
 	DisruptionTemplate DisruptionSpec `json:"disruptionTemplate"`
 }
@@ -49,21 +55,25 @@ type DisruptionScheduleSpec struct {
 // Disruptions are intended to exist semi-permanently, and thus appropriate targets can only be other long-lived resources,
 // such as statefulsets or deployment.
 type TargetResourceSpec struct {
-	// Kind specifies the type of the long-lived resource. Allowed values: "deployment", "statefulset".
 	// +kubebuilder:validation:Enum=deployment;statefulset
+	// +kubebuilder:validation:Required
 	// +ddmark:validation:Enum=deployment;statefulset
+	// +ddmark:validation:Required=true
+	// Kind specifies the type of the long-lived resource. Allowed values: "deployment", "statefulset".
 	Kind string `json:"kind"`
 
+	// +kubebuilder:validation:Required
+	// +ddmark:validation:Required=true
 	// Name specifies the name of the specific instance of the long-lived resource to be targeted.
 	Name string `json:"name"`
 }
 
 type DisruptionScheduleStatus struct {
 	// The last time when the disruption was last successfully scheduled.
-	// +optional
+	// +nullable
 	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
 
 	// Time when the target resource was previously missing.
-	// +optional
+	// +nullable
 	TargetResourcePreviouslyMissing *metav1.Time `json:"TargetResourcePreviouslyMissing,omitempty"`
 }
