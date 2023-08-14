8dce2a704895f3693201b1c7f7e1bc7f64e37e70
Diyara Bissembayeva <diyara.bissembayeva@datadoghq.com>
2023-08-13 23:44:59 -0400

    Check PodSpec hashes and refactor disruption rollouts fetching

--- watchers/deployment_handler.go
+++ watchers/deployment_handler.go
@@ -2,10 +2,14 @@ package watchers
 
 import (
 	context "context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
 
 	chaosv1beta1 "github.com/DataDog/chaos-controller/api/v1beta1"
 	"go.uber.org/zap"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -30,7 +34,7 @@ func (h DeploymentHandler) OnAdd(_ interface{}) {
 // OnUpdate is a handler function for the update of a deployment
 func (h DeploymentHandler) OnUpdate(oldObj, newObj interface{}) {
 	// Convert oldObj and newObj to Deployment objects
-	_, okOldDeployment := oldObj.(*appsv1.Deployment)
+	oldDeployment, okOldDeployment := oldObj.(*appsv1.Deployment)
 	newDeployment, okNewDeployment := newObj.(*appsv1.Deployment)
 
 	// If both old and new are not deployments, do nothing
@@ -42,18 +46,40 @@ func (h DeploymentHandler) OnUpdate(oldObj, newObj interface{}) {
 	if !h.hasAssociatedDisruptionRollout(newDeployment) {
 		return
 	}
+
+	// If pod spec of deployment hasn't changed, do nothing
+	oldHash := hashPodSpec(&oldDeployment.Spec.Template.Spec)
+	newHash := hashPodSpec(&newDeployment.Spec.Template.Spec)
+
+	if oldHash != newHash {
+		return
+	}
 }
 
-func (h DeploymentHandler) hasAssociatedDisruptionRollout(deployment *appsv1.Deployment) bool {
+func (h DeploymentHandler) fetchAssociatedDisruptionRollouts(deployment *appsv1.Deployment) (*chaosv1beta1.DisruptionRolloutList, error) {
 	indexedValue := "Deployment" + "-" + deployment.Namespace + "-" + deployment.Name
 
 	disruptionRollouts := &chaosv1beta1.DisruptionRolloutList{}
-
 	err := h.Client.List(context.TODO(), disruptionRollouts, client.MatchingFields{"targetResource": indexedValue})
+
 	if err != nil {
 		h.log.Errorw("unable to fetch DisruptionRollouts using index", "error", err, "indexedValue", indexedValue)
+		return nil, err
+	}
+
+	return disruptionRollouts, nil
+}
+
+func (h DeploymentHandler) hasAssociatedDisruptionRollout(deployment *appsv1.Deployment) bool {
+	disruptionRollouts, err := h.fetchAssociatedDisruptionRollouts(deployment)
+	if err != nil {
 		return false
 	}
 
 	return len(disruptionRollouts.Items) > 0
+}
+
+func hashPodSpec(spec *corev1.PodSpec) string {
+	data, _ := json.Marshal(spec)
+	return hex.EncodeToString(md5.New().Sum(data))
 }
