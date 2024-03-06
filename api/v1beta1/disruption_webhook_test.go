327dddfa46ab34023043c359c4a491da4fe43513
Philip Thompson <philip.thompson@datadoghq.com>
2024-03-06 13:55:58 -0500

    why does it think these are different

--- api/v1beta1/disruption_webhook_test.go
+++ api/v1beta1/disruption_webhook_test.go
@@ -103,40 +103,33 @@ var _ = Describe("Disruption", func() {
 		Describe("ValidateCreate-only invariants shouldn't affect Update", func() {
 			When("triggers.*.notBefore is in the past", func() {
 				It("triggers.inject should not return an error", func() {
-					newTime := metav1.NewTime(time.Now().Add(time.Minute * 5 * -1))
-					newDisruption.Spec.Duration = "30m"
-					newDisruption.Spec.Triggers = DisruptionTriggers{
+					triggers := DisruptionTriggers{
 						Inject: DisruptionTrigger{
-							NotBefore: newTime,
+							NotBefore: metav1.NewTime(time.Now().Add(time.Minute * 5 * -1)),
 						},
 					}
 
+					newDisruption.Spec.Duration = "30m"
+					newDisruption.Spec.Triggers = triggers
+
 					oldDisruption.Spec.Duration = "30m"
-					oldDisruption.Spec.Triggers = DisruptionTriggers{
-						Inject: DisruptionTrigger{
-							NotBefore: newTime,
-						},
-					}
+					oldDisruption.Spec.Triggers = triggers
 
 					err := newDisruption.ValidateUpdate(oldDisruption)
 					Expect(err).Should(Succeed())
 				})
 
 				It("triggers.createPods should not return an error", func() {
-					newTime := metav1.NewTime(time.Now().Add(time.Hour * -1))
-					newDisruption.Spec.Duration = "30m"
-					newDisruption.Spec.Triggers = DisruptionTriggers{
+					triggers := DisruptionTriggers{
 						CreatePods: DisruptionTrigger{
-							NotBefore: newTime,
+							NotBefore: metav1.NewTime(time.Now().Add(time.Hour * -1)),
 						},
 					}
+					newDisruption.Spec.Duration = "30m"
+					newDisruption.Spec.Triggers = triggers
 
 					oldDisruption.Spec.Duration = "30m"
-					oldDisruption.Spec.Triggers = DisruptionTriggers{
-						Inject: DisruptionTrigger{
-							NotBefore: newTime,
-						},
-					}
+					oldDisruption.Spec.Triggers = triggers
 
 					err := newDisruption.ValidateUpdate(oldDisruption)
 					Expect(err).Should(Succeed())
