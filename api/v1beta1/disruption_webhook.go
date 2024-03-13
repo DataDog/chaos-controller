572b4ff14427e727f2e078bcce875d4928686b67
Philip Thompson <philip.thompson@datadoghq.com>
2024-03-13 16:09:49 -0400

    set a logger in the webhook?

--- api/v1beta1/disruption_webhook.go
+++ api/v1beta1/disruption_webhook.go
@@ -16,6 +16,7 @@ import (
 	cloudtypes "github.com/DataDog/chaos-controller/cloudservice/types"
 	"github.com/DataDog/chaos-controller/ddmark"
 	"github.com/DataDog/chaos-controller/utils"
+	"github.com/go-logr/zapr"
 	"go.opentelemetry.io/otel/trace"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -89,6 +90,8 @@ func (r *Disruption) SetupWebhookWithManager(setupWebhookConfig utils.SetupWebho
 	}
 
 	permittedUserGroupWarningString = strings.Join(setupWebhookConfig.PermittedUserGroups, ",")
+
+	ctrl.SetLogger(zapr.NewLogger(logger.Desugar()))
 
 	return ctrl.NewWebhookManagedBy(setupWebhookConfig.Manager).
 		For(r).
