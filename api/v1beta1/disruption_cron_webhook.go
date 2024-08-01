c0477c75e2ad4370a5bcf5eaa7b5e505bc10d23b
Philip Thompson <philip.thompson@datadoghq.com>
2024-08-01 16:50:25 -0400

    fix test panic?

--- api/v1beta1/disruption_cron_webhook.go
+++ api/v1beta1/disruption_cron_webhook.go
@@ -67,7 +67,7 @@ func (d *DisruptionCron) ValidateCreate() (admission.Warnings, error) {
 	}
 
 	// send informative event to disruption cron to broadcast
-	disruptionCronWebhookRecorder.AnnotatedEventf(d, d.Annotations, Events[EventDisruptionCronCreated].Type, string(EventDisruptionCronCreated), Events[EventDisruptionCronCreated].OnDisruptionTemplateMessage)
+	disruptionCronWebhookRecorder.AnnotatedEventf(d, d.GetAnnotations(), Events[EventDisruptionCronCreated].Type, string(EventDisruptionCronCreated), Events[EventDisruptionCronCreated].OnDisruptionTemplateMessage)
 
 	return nil, nil
 }
