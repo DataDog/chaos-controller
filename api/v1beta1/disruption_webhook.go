896ab330a788cfb624d7be4ad684e834ede7a512
Philip Thompson <philip.thompson@datadoghq.com>
2024-11-21 10:04:54 -0500

    Add a Spec.SetDefaults method

--- api/v1beta1/disruption_webhook.go
+++ api/v1beta1/disruption_webhook.go
@@ -114,8 +114,9 @@ var _ webhook.Defaulter = &Disruption{}
 func (r *Disruption) Default() {
 	if r.Spec.Duration.Duration() == 0 {
 		logger.Infow(fmt.Sprintf("setting default duration of %s in disruption", defaultDuration), "instance", r.Name, "namespace", r.Namespace)
-		r.Spec.Duration = DisruptionDuration(defaultDuration.String())
 	}
+
+	r.Spec.SetDefaults()
 }
 
 //+kubebuilder:webhook:webhookVersions={v1},path=/validate-chaos-datadoghq-com-v1beta1-disruption,mutating=false,failurePolicy=fail,sideEffects=None,groups=chaos.datadoghq.com,resources=disruptions,verbs=create;update;delete,versions=v1beta1,name=vdisruption.kb.io,admissionReviewVersions={v1,v1beta1}
