001bb661cc46462820814a9fd60264f4ddb5d17e
Claire Chong <claire.chong@datadoghq.com>
2025-01-06 13:52:25 +0100

    We're in 2025 🎉

--- webhook/webhook_suite_test.go
+++ webhook/webhook_suite_test.go
@@ -1,14 +1,15 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2024 Datadog, Inc.
+// Copyright 2025 Datadog, Inc.
 
 package webhook_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
 func TestWebhook(t *testing.T) {
