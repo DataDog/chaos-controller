6f9c207f08441da014f880ec0291ff2f1c45c7f5
Philip Thompson <philip.thompson@datadoghq.com>
2025-02-10 16:45:36 -0500

    disk pressure

--- api/v1beta1/disk_pressure.go
+++ api/v1beta1/disk_pressure.go
@@ -50,6 +50,15 @@ func (s *DiskPressureSpec) GenerateArgs() []string {
 }
 
 func (s *DiskPressureSpec) Explain() string {
-	explanation := fmt.Sprintf("spec.diskPressure will throttle io ")
+	explanation := fmt.Sprintf("spec.diskPressure will throttle io on the device mounted to the path %s, limiting it to ", s.Path)
+
+	if s.Throttling.ReadBytesPerSec != nil {
+		explanation += fmt.Sprintf("%d read bytes per second ", *s.Throttling.ReadBytesPerSec)
+	}
+
+	if s.Throttling.WriteBytesPerSec != nil {
+		explanation += fmt.Sprintf("%d write bytes per second.", *s.Throttling.WriteBytesPerSec)
+	}
+
 	return explanation
 }
