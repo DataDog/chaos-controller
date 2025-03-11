62047e064d80d2f45ea80e0c757dbd2b768a906c
Claire Chong <claire.chong@datadoghq.com>
2025-03-11 14:38:10 +0100

    add validation metrics

--- api/v1beta1/disruption_cron_webhook.go
+++ api/v1beta1/disruption_cron_webhook.go
@@ -121,6 +121,11 @@ func (d *DisruptionCron) ValidateCreate() (_ admission.Warnings, err error) {
 		return nil, err
 	}
 
+	// send validation metric
+	if err := metricsSink.MetricValidationCreated(metricTags); err != nil {
+		log.Errorw("error sending a metric", "error", err)
+	}
+
 	// send informative event to disruption cron to broadcast
 	d.emitEvent(EventDisruptionCronCreated)
 
@@ -163,6 +168,11 @@ func (d *DisruptionCron) ValidateUpdate(oldObject runtime.Object) (_ admission.W
 		}
 	}
 
+	// send validation metric
+	if err := metricsSink.MetricValidationUpdated(metricTags); err != nil {
+		log.Errorw("error sending a metric", "error", err)
+	}
+
 	// send informative event to disruption cron to broadcast
 	d.emitEvent(EventDisruptionCronUpdated)
 
@@ -176,6 +186,11 @@ func (d *DisruptionCron) ValidateDelete() (warnings admission.Warnings, err erro
 
 	// During the validation of the deletion the timestamp does not exist so we need to set it before emitting the event
 	d.DeletionTimestamp = &metav1.Time{Time: time.Now()}
+
+	// send validation metric
+	if err := metricsSink.MetricValidationDeleted(d.getMetricsTags()); err != nil {
+		log.Errorw("error sending a metric", "error", err)
+	}
 
 	// send informative event to disruption cron to broadcast
 	d.emitEvent(EventDisruptionCronDeleted)
