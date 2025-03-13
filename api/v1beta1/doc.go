2e77f9ca8582037b9b80aeaa116223209e6067fe
Philip Thompson <philip.thompson@datadoghq.com>
2025-03-13 13:30:31 +0100

    Fix "package comment" header lint (#972)
    * Add a new line after our autogenerated header
    
    * Update all headers to include the new line
    
    * Add header new line to more files

--- api/v1beta1/doc.go
+++ api/v1beta1/doc.go
@@ -2,5 +2,6 @@
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025 Datadog, Inc.
+
 // +groupName=chaos.datadoghq.com
 package v1beta1
