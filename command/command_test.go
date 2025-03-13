8cda41b8e56beb7f7f90b761c3d3b05301c46700
Philip Thompson <philip.thompson@datadoghq.com>
2025-03-13 11:16:13 +0100

    Update all headers to include the new line

--- command/command_test.go
+++ command/command_test.go
@@ -2,6 +2,7 @@
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025 Datadog, Inc.
+
 package command
 
 import (
