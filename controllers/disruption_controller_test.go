74e5b24d8c6da841627ca0b47b2f41d902284719
Diyara Bissembayeva <diyara.bissembayeva@datadoghq.com>
2025-10-27 19:06:27 -0400

    fix: adjust test expectations

--- controllers/disruption_controller_test.go
+++ controllers/disruption_controller_test.go
@@ -92,7 +92,7 @@ var _ = Describe("Disruption Controller", func() {
 		})
 
 		It("should only select half of all pods", func(ctx SpecContext) {
-			ExpectChaosPods(ctx, disruption, 4)
+			ExpectChaosPods(ctx, disruption, 3)
 		})
 	})
 
@@ -106,7 +106,7 @@ var _ = Describe("Disruption Controller", func() {
 		})
 
 		It("should only select half of all pods", func(ctx SpecContext) {
-			ExpectChaosPods(ctx, disruption, 4)
+			ExpectChaosPods(ctx, disruption, 3)
 		})
 	})
 
@@ -217,10 +217,10 @@ var _ = Describe("Disruption Controller", func() {
 	Context("target one pod and one container only", func() {
 		It("should target all the selected pods", func(ctx SpecContext) {
 			By("Ensuring that the inject pod has been created")
-			ExpectChaosPods(ctx, disruption, 4)
+			ExpectChaosPods(ctx, disruption, 3)
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			ExpectChaosInjectors(ctx, disruption, 4)
+			ExpectChaosInjectors(ctx, disruption, 3)
 		})
 	})
 
@@ -231,10 +231,10 @@ var _ = Describe("Disruption Controller", func() {
 
 		It("should target all the selected pods", func(ctx SpecContext) {
 			By("Ensuring that the chaos pods have been created")
-			ExpectChaosPods(ctx, disruption, 8)
+			ExpectChaosPods(ctx, disruption, 6)
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			ExpectChaosInjectors(ctx, disruption, 8)
+			ExpectChaosInjectors(ctx, disruption, 6)
 		})
 	})
 
@@ -248,10 +248,10 @@ var _ = Describe("Disruption Controller", func() {
 			InjectPodsAndDisruption(ctx, *disruptionTwo, skipSecondPod)
 
 			By("Ensuring that no extra chaos pods have been created")
-			ExpectChaosPods(ctx, disruption, 8)
+			ExpectChaosPods(ctx, disruption, 6)
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			ExpectChaosInjectors(ctx, disruption, 8)
+			ExpectChaosInjectors(ctx, disruption, 6)
 		})
 	})
 
@@ -262,10 +262,10 @@ var _ = Describe("Disruption Controller", func() {
 
 		It("should target all the selected pods", func(ctx SpecContext) {
 			By("Ensuring that the inject pod has been created")
-			ExpectChaosPods(ctx, disruption, 4)
+			ExpectChaosPods(ctx, disruption, 3)
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			ExpectChaosInjectors(ctx, disruption, 4)
+			ExpectChaosInjectors(ctx, disruption, 3)
 		})
 	})
 
@@ -278,10 +278,10 @@ var _ = Describe("Disruption Controller", func() {
 
 		It("should target all the selected pods", func(ctx SpecContext) {
 			By("Ensuring that the chaos pods have been created")
-			ExpectChaosPods(ctx, disruption, 8)
+			ExpectChaosPods(ctx, disruption, 6)
 
 			By("Ensuring that the chaos pods have correct number of targeted containers")
-			ExpectChaosInjectors(ctx, disruption, 12)
+			ExpectChaosInjectors(ctx, disruption, 9)
 		})
 	})
 
