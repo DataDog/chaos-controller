ab3dfc5c3f07593742344d4911720ec66f2b20c5
Jed Needle <jed.needle@datadoghq.com>
2025-10-27 14:18:15 +0100

    add back in the default header so circle does not fail the header check

--- cpuset/cpuset_test.go
+++ cpuset/cpuset_test.go
@@ -1,8 +1,21 @@
 /*
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2025 Datadog, Inc.
+Copyright 2017 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+This file was copied directly from k8s.io/kubernetes v1.20.2. It is not importable in a normal way, as kubernetes/pkg/kubelet isn't meant to be imported,
+ doing so requires `replace` statements that make it difficult to import the chaos-controller from other modules.
 */
 
 package cpuset
