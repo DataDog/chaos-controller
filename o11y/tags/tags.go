0166d1805abfe06c0d121454951a8208d1fa4f3d
Jed Needle <jed.needle@datadoghq.com>
2025-11-17 07:51:42 -0500

    changes to tags, main and dockerfile for libfaketime

--- o11y/tags/tags.go
+++ o11y/tags/tags.go
@@ -115,6 +115,7 @@ const (
 	// Generic fields
 	ConfigKey          = "config"
 	DataKey            = "data"
+	EnvFileKey         = "env_file"
 	HostKey            = "host"
 	IndexedValueKey    = "indexed_value"
 	KindKey            = "kind"
@@ -158,6 +159,7 @@ const (
 	MaxRunsKey                    = "max_runs"
 	NextRunKey                    = "next_run"
 	NowKey                        = "now"
+	OffsetKey                     = "offset"
 	PauseDurationKey              = "pause_duration"
 	RemainingDurationKey          = "remaining_duration"
 	RequeueAfterKey               = "requeue_after"
