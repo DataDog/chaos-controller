9092c17770764ba3195e7b09c2e383a6c3b48e13
Claire Chong <claire.chong@datadoghq.com>
2026-01-13 15:20:39 +0100

    [chaosplt] switch to 2026

--- api/v1beta1/install/install.go
+++ api/v1beta1/install/install.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2025 Datadog, Inc.
+// Copyright 2026 Datadog, Inc.
 
 // Package install installs the experimental API group, making it available as
 // an option to all of the API encoding/decoding machinery.
